cb9dd3b30d1b38a977aac5cbd85ce6d4f901119a
p-Cipriano <61161977+p-Cipriano@users.noreply.github.com>
2021-03-02 14:07:36 -0300

    Update Encomenda.cpp

--- Encomenda.cpp
+++ Encomenda.cpp
@@ -2,49 +2,50 @@
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
 		
-    Encomenda::Encomenda(bool prioridade, int peso, int custokg, Cliente Remetente, Cliente Destinatario){
-			this->relampago=prioridade;
-			this->PESO=peso;
-			this->CUSTOkg=custokg;
-			this->remetente=Remetente;
-			this->dest=Destinatario;
-		}
+	Encomenda::Encomenda(bool prioridade, int peso, int custokg, Cliente Remetente, Cliente Destinatario){
+		this->relampago=prioridade;
+		this->PESO=peso;
+		this->CUSTOkg=custokg;
+		this->remetente=Remetente;
+		this->dest=Destinatario;
+	}
 		
-		double Encomenda::calcula(){
+	double Encomenda::calcula(){
 			
-			this->T = this->PESO * this->CUSTOkg;
+		this->T = this->PESO * this->CUSTOkg;
 			
-			if(this->relampago){
+		if(this->relampago){
 				
-				this->T *= 1.25;
-			}
-			
-			return this->T;
+			this->T *= 1.25;
 		}
-		void Encomenda::print(){
+			
+		return this->T;
+	}
+	void Encomenda::print(){
 
-			cout << "[Remetente]" << endl;
-			this->remetente.print();
-			cout << "[Destinatário]" << endl;
-			this->dest.print();
+		cout << "[Remetente]" << endl;
+		this->remetente.print();
+		cout << "[Destinatário]" << endl;
+		this->dest.print();
 			
-			if(!this->relampago){
-				cout << "[Encomenda Normal]" << endl;
-				cout << "  Peso: " << this->PESO << endl
-					<< "  Custo por kg: " << this->CUSTOkg << endl
-					<< "  Custo total: " << this->T << endl;
-			}
+		if(!this->relampago){
+			cout << "[Encomenda Normal]" << endl;
+			cout << "  Peso: " << this->PESO << endl
+				<< "  Custo por kg: " << this->CUSTOkg << endl
+				<< "  Custo total: " << this->T << endl;
+		}
 			
-			else{
-				cout << "[Encomenda Relâmpago]" << endl;
-				cout << "  Peso: " << this->PESO << endl
-					<< "  Custo por kg: " << this->CUSTOkg << endl
-					<< "  Taxa adicional: " << 0.25 << endl
-					<< "  Custo total: " << this->T << endl;
+		else{
+			cout << "[Encomenda Relâmpago]" << endl;
+			cout << "  Peso: " << this->PESO << endl
+				<< "  Custo por kg: " << this->CUSTOkg << endl
+				<< "  Taxa adicional: " << 0.25 << endl
+				<< "  Custo total: " << this->T << endl;
 				
 		}
-		bool Encomenda::prioridade(){
+	}
+	bool Encomenda::prioridade(){
 			
-			return this->relampago;
-		}
+		return this->relampago;
+	}
 	
