ce61d125004fa5b54340aab76d44c703cb1d305c
p-Cipriano <61161977+p-Cipriano@users.noreply.github.com>
2021-03-02 13:54:24 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -8,51 +8,10 @@ class Encomenda{
 	
 	public:
 		
-		Encomenda(bool prioridade, int peso, int custokg, Cliente Remetente, Cliente Destinatario){
-			this->relampago=prioridade;
-			this->PESO=peso;
-			this->CUSTOkg=custokg;
-			this->remetente=Remetente;
-			this->dest=Destinatario;
-		}
-		
-		double calcula(){
-			
-			this->T = this->PESO * this->CUSTOkg;
-			
-			if(this->relampago){
-				
-				this->T *= 1.25;
-			}
-			
-			return this->T;
-		}
-		void print(){
-
-			cout << "[Remetente]" << endl;
-			this->remetente.print();
-			cout << "[Destinatário]" << endl;
-			this->dest.print();
-			
-			if(!this->relampago){
-				cout << "[Encomenda Normal]" << endl;
-				cout << "  Peso: " << this->PESO << endl
-					<< "  Custo por kg: " << this->CUSTOkg << endl
-					<< "  Custo total: " << this->T << endl;
-			}
-			
-			else{
-				cout << "[Encomenda Relâmpago]" << endl;
-				cout << "  Peso: " << this->PESO << endl
-					<< "  Custo por kg: " << this->CUSTOkg << endl
-					<< "  Taxa adicional: " << 0.25 << endl
-					<< "  Custo total: " << this->T << endl;
-				
-		}
-		bool prioridade(){
-			
-			return this->relampago;
-		}
+		Encomenda(bool prioridade, int peso, int custokg, Cliente Remetente, Cliente Destinatario);
+		double calcula();
+		void print();
+		bool prioridade();
 	
 	private:
 		
