3e5857e21bdf74516b1110d94a4bdb1a464f6a61
Lucas-Sarquis <64432911+Lucas-Sarquis@users.noreply.github.com>
2021-03-03 11:48:59 -0300

    Update EncomendaNormal.hpp
    Remoção de métodos já declarados e especificados em Encomenda.hpp; melhora da visibilidade; identação do código e definição do método printNormal;

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -5,27 +5,19 @@
 #include "Cliente.hpp"
 using namespace std;
 
-class EncomendaNormal: public Encomenda{
+class EncomendaNormal : public Encomenda
+{
 
-	public:
-		
-	double calcula(){
+public:
+    void printNormal()
+    {
 
-		double x = PESO * CUSTOkg;
-
-		return x;
-	}
-
-	void print(){
-
-		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
-
-	}
-
+        Encomenda::print();
+        std::cout << "[Encomenda Normal]" << endl;
+        std::cout << "  Peso: " << getPESO() << endl
+                  << "  Custo por kg: " << getCUSTOkg() << endl
+                  << "  Custo total: " << getTotal() << endl;
+    }
 };
 
 #endif
