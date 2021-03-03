ddbaa712810493125e336ecb56ae04e8748499e4
Lucas-Sarquis <64432911+Lucas-Sarquis@users.noreply.github.com>
2021-03-03 11:52:01 -0300

    Update Encomenda.hpp
    Privando os atributos essenciais da Encomenda; definição de métodos get e set para tais atributos privados; definição dos métodos calcula() e print(); identação do código e melhora da visibilidade.

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -4,24 +4,60 @@
 #include "Cliente.hpp"
 using namespace std;
 
-class Encomenda{
+class Encomenda
+{
+    /**Variáveis que contém as características da encomenda como privadas**/
+private:
+    double PESO = 0.0;
+    double CUSTOkg = 0.0;
+    double Total = 0.0;
 
-	public:
+public:
+    Cliente remetente;
+    Cliente dest;
 
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
-			
-		void print(){
+    double getTotal()
+    {
+        this->Total = calcula();
+        return Total;
+    }
 
-			std::cout << "[Remetente]" << endl;
-			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
-		}
+    double getPESO()
+    {
+        return PESO;
+    }
 
+    void setPESO(double weith)
+    {
+        this->PESO = weith;
+    }
+
+    double getCUSTOkg()
+    {
+        return CUSTOkg;
+    }
+
+    void setCUSTOkg(double cost)
+    {
+        this->CUSTOkg = cost;
+    }
+
+    void print()
+    {
+
+        std::cout << "[Remetente]" << endl;
+        remetente.print();
+        std::cout << "[Destinatário]" << endl;
+        dest.print();
+    }
+
+    virtual double calcula()
+    {
+
+        double x = PESO * CUSTOkg;
+
+        return x;
+    }
 };
 
 #endif
