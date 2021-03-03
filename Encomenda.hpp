5c131b9356dccd8e12c210535a69492348503659
dmrohrs <77017219+dmrohrs@users.noreply.github.com>
2021-03-03 17:54:58 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -1,27 +1,44 @@
+
 #ifndef Encomenda_H
 #define Encomenda_H
 
 #include "Cliente.hpp"
-using namespace std;
 
-class Encomenda{
+const int taxa = 0.25;
 
-	public:
+namespace Compra
+{
+    class Encomenda
+    {
 
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
-			
-		void print(){
+    public:
+        double getPeso();
+        double getCustokg();
+        virtual double calcula() = 0;
+        virtual void print();
 
-			std::cout << "[Remetente]" << endl;
-			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
-		}
+    private:
+        double PESO = 0.0;
+        double CUSTOkg = 0.0;
+        double T = 0.0;
+        Cliente remetente;
+        Cliente destinatario;
+    };
 
-};
+    class EncomendaNormal : public Encomenda
+    {
 
+    public:
+        double calcula();
+        void print() overrride;
+    };
+
+    class EncomendaRelampago : public Encomenda
+    {
+
+    public:
+        double calcula();
+        void print();
+    };
+}
 #endif
