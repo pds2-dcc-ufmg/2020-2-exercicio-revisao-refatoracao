ca93bdc1e5c8b6e82f6ef34e3aa3e0545bee89f4
Joao Gomes <jgomesjunqueiras@gmail.com>
2021-03-02 22:33:59 -0800

    Classe Cliente modificada

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,57 @@
+#include "Cliente.hpp"
 #include <iostream>
 #include <string>
-#include "Cliente.hpp"
 
-void Cliente::print(){
+Cliente::Cliente(){};
+Cliente::Cliente(std::string _nome,
+                 std::string _endereco,
+                 std::string _cidade,
+                 std::string _estado,
+                 std::string _cep) {
+    nome = _nome;
+    endereco = _endereco;
+    cidade = _cidade;
+    estado = _estado;
+    cep = _cep;
+}
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+std::string Cliente::getNome() {
+    return nome;
+}
+std::string Cliente::getEndereco() {
+    return endereco;
+}
+std::string Cliente::getCidade() {
+    return cidade;
+}
+std::string Cliente::getEstado() {
+    return estado;
+}
+std::string Cliente::getCep() {
+    return nome;
+}
 
+void Cliente::setNome(std::string _nome) {
+    nome = _nome;
+}
+void Cliente::setEndereco(std::string _endereco) {
+    endereco = _endereco;
+}
+void Cliente::setCidade(std::string _cidade) {
+    cidade = _cidade;
+}
+void Cliente::setEstado(std::string _estado) {
+    estado = _estado;
+}
+
+void Cliente::setCep(std::string _cep) {
+    cep = _cep;
+}
+
+void Cliente::print() {
+    std::cout << "  Nome: " << nome << std::endl
+              << "  Endereço: " << endereco << std::endl
+              << "  Cidade: " << cidade << std::endl
+              << "  Estado: " << estado << std::endl
+              << "  cep: " << cep << std::endl;
 }
--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,37 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
-class Cliente{
+class Cliente {
 
-	public:
+private:
+    std::string nome;
+    std::string endereco;
+    std::string cidade;
+    std::string estado;
+    std::string cep;
 
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+public:
+    Cliente();
+    Cliente(std::string _nome,
+            std::string _endereco,
+            std::string _cidade,
+            std::string _estado,
+            std::string _cep);
+
+    std::string getNome();
+    std::string getEndereco();
+    std::string getCidade();
+    std::string getEstado();
+    std::string getCep();
+
+    void setNome(std::string);
+    void setEndereco(std::string);
+    void setCidade(std::string);
+    void setEstado(std::string);
+    void setCep(std::string);
+
+    void print(); // imprime na tela os dados de um cliente cadastrado
 };
 
 #endif
