1f54f1857325e7ba2713f76c0b4018aaf7bdeaa8
Tusoba <64483113+Tusoba@users.noreply.github.com>
2021-03-03 12:05:29 -0300

    Update EncomendaRelampago.hpp

--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -5,29 +5,44 @@
 #include "Cliente.hpp"
 using namespace std;
 
-class EncomendaRelampago: public Encomenda{
+class EncomendaRelampago : public Encomenda
+{
+private:
+	double taxaRelampago = 1.25; // taxa extra para entregas relampago
 
-	public:
+public:
+	EncomendaRelampago() {} // construtor padrão
+	EncomendaRelampago(double _peso, double _custokg, Cliente _remetente, Cliente _dest)
+	{
+		this->PESO = _peso;
+		this->CUSTOkg = _custokg;
+		this->remetente = _remetente;
+		this->dest = _dest;
+	} // construtor
 
-		double calcula(){
+	double calcula() override
+	{
 
-			double x = PESO * CUSTOkg;
-			x += x * 0.25;
-			
-			return x;
-		}
+		double x = PESO * CUSTOkg;
+		x = x * taxaRelampago;
+		return x;
 
-		void print(){
+	} // função para calcular o preço da entrega
 
-			Encomenda::print();
-			std::cout << "[Encomenda Relâmpago]" << endl;
-			std::cout << "  Peso: " << PESO << endl
-				<< "  Custo por kg: " << CUSTOkg << endl
-				<< "  Taxa adicional: " << 0.25 << endl
-				<< "  Custo total: " << T << endl;
+	void printEncomenda() override
+	{
 
-		}
+		std::cout << "[Remetente]" << endl;
+		remetente.printCliente();
+		std::cout << "[Destinatário]" << endl;
+		dest.printCliente();
+		std::cout << "[Encomenda Relâmpago]" << endl;
+		std::cout << "  Peso: " << PESO << endl
+				  << "  Custo por kg: " << CUSTOkg << endl
+				  << "  Taxa adicional: " << 0.25 << endl
+				  << "  Custo total: " << custoTotal << endl;
 
+	} // função para imprimir dados da entrega Relanpago
 };
 
 #endif
