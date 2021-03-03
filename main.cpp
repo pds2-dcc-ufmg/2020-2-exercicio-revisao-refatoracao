389104b292811bb760b261114f2c9b449fcf8391
p-Cipriano <61161977+p-Cipriano@users.noreply.github.com>
2021-03-03 09:53:31 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,55 +5,58 @@
 
 int main(){
 	
-	Cliente cli[8];
+	Cliente cliente[8];
 	
-	cli[0].set("Thales","Rua dos Pré-Socráticos", "Miletus", "Ionia","548 a.C.");
-	cli[1].set("Aristóteles", "Avenida do Meio-Termo", "Atenas", "Ática", "384 a.C.");
-	cli[2].set("Platão", "Praça das Formas", "Atenas", "Ática", "348 a.C.");
-	cli[3].set("Sócrates", "Rua do Elenchus", "Atenas", "Ática", "399 a.C.");
-	cli[4].set("Pitágoras", "Praça dos Quadrados dos Catetos", "Samos", "Egeu", "571 a.C.");
-	cli[5].set("Parmênides", "Rua do Não Ser", "Eleia", "Magna Grécia", "460 a.C.");
-	cli[6].set("Empédocles", "Rua dos Quatro Elementos", "Agrigento", "Sicília", "495 a.C.");
-	cli[7].set("Anaxágoras", "Avenida da Mente Cósmica", "Clazômenas", "Jónia", "499 a.C.");
+	cliente[0].set("Thales","Rua dos Pré-Socráticos", "Miletus", "Ionia","548 a.C.");
+	cliente[1].set("Aristóteles", "Avenida do Meio-Termo", "Atenas", "Ática", "384 a.C.");
+	cliente[2].set("Platão", "Praça das Formas", "Atenas", "Ática", "348 a.C.");
+	cliente[3].set("Sócrates", "Rua do Elenchus", "Atenas", "Ática", "399 a.C.");
+	cliente[4].set("Pitágoras", "Praça dos Quadrados dos Catetos", "Samos", "Egeu", "571 a.C.");
+	cliente[5].set("Parmênides", "Rua do Não Ser", "Eleia", "Magna Grécia", "460 a.C.");
+	cliente[6].set("Empédocles", "Rua dos Quatro Elementos", "Agrigento", "Sicília", "495 a.C.");
+	cliente[7].set("Anaxágoras", "Avenida da Mente Cósmica", "Clazômenas", "Jónia", "499 a.C.");
 	
-	Encomenda enc[8];
+	Encomenda encomenda[8];
 	
-	enc[0].set(0,5,12,cli[0],cli[1]);
-	enc[1].set(0,10,12,cli[1],cli[2]);
-	enc[2].set(0,7,12,cli[2],cli[3]);
-	enc[3].set(0,2,12,cli[3],cli[4]);
-	enc[4].set(0,3,12,cli[4],cli[5]);
-	enc[5].set(1,13,18,cli[5],cli[6]);
-	enc[6].set(1,6,18,cli[6],cli[7]);
-	enc[7].set(1,8,18,cli[7],cli[0]);
+	encomenda[0].set(0,5,12,cli[0],cli[1]);
+	encomenda[1].set(0,10,12,cli[1],cli[2]);
+	encomenda[2].set(0,7,12,cli[2],cli[3]);
+	encomenda[3].set(0,2,12,cli[3],cli[4]);
+	encomenda[4].set(0,3,12,cli[4],cli[5]);
+	encomenda[5].set(1,13,18,cli[5],cli[6]);
+	encomenda[6].set(1,6,18,cli[6],cli[7]);
+	encomenda[7].set(1,8,18,cli[7],cli[0]);
 
 	std::cout << "\n>> Relatório de encomendas <<" << std::endl;
 
-	int quantN = 0;
-	double custN = 0;
-	int quantR = 0;
-	double custR = 0;
+	int QuantN = 0;
+	double CustoN = 0;
+	int QuantR = 0;
+	double CustoR = 0;
 	
 	for(int i=0; i<8; i++){
-		enc[i].calcula();
-		enc[i].print();
+		
+		encomenda[i].calcula();
+		encomenda[i].print();
+		
 		std::cout << std::endl;
-		if(enc[i].prioridade()){
-			quantR++;
-			custR+=enc[i].calcula();
+		
+		if(encomenda[i].prioridade()){
+			QuantN++;
+			CustoR+=encomenda[i].calcula();
 		}
 		else{
-			quantN++;
-			custN+=enc[i].calcula();
+			QuantiaR++;
+			CustoN+=encomenda[i].calcula();
 		}
 	}
 	
 	std::cout << "\n>> Encomendas Normais <<" 
-		 << "\nQuantidade: " << quantN
-		 << "\nValor Total: " << custN
+		 << "\nQuantidade: " << QuantN
+		 << "\nValor Total: " << CustoN
 		 << std::endl
 		 << "\n>> Encomendas Relâmpago <<" 
-		 << "\nQuantidade: " << quantR
-		 << "\nValor Total: " << custR
+		 << "\nQuantidade: " << QuantR
+		 << "\nValor Total: " << CustoR
 		 << std::endl;
 }
