41e39fcfc6a18a1b1a47a573e5c25f90308db0ab
LRiosB <36747760+LRiosB@users.noreply.github.com>
2021-03-04 20:49:41 -0300

    Add files via upload
    Arquivos modificados

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -7,20 +7,20 @@ using namespace std;
 class Encomenda{
 
 	public:
+		Encomenda(){};
+
+		virtual void print() = 0;
+
+		virtual double calcula() = 0;
+	
+	protected:
 
 		double PESO = 0.0;
 		double CUSTOkg = 0.0;
 		double T = 0.0;
 		Cliente remetente;
 		Cliente dest;
-			
-		void print(){
-
-			std::cout << "[Remetente]" << endl;
-			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
-		}
+	
 
 };
 
