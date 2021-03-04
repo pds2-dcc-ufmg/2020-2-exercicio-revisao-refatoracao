125a9a591f415d3dd1ef0983443d4a7059cdf259
unknown <guilherme.olivsilva01@gmail.com>
2021-03-04 00:37:47 -0300

    Revisao de codigo

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
-#include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+void Cliente::set_nome(std::string _nome) {
+	nome = _nome;
+}
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+std::string Cliente::get_nome() {
+	return nome;
+}
 
+void Cliente::set_endereco(std::string _endereco) {
+	endereco = _endereco;
+}
+
+std::string Cliente::get_endereco() {
+	return endereco;
+}
+
+void Cliente::set_cidade(std::string _cidade) {
+	cidade = _cidade;
+}
+
+std::string Cliente::get_cidade() {
+	return cidade;
+}
+
+void Cliente::set_estado(std::string _estado) {
+	estado = _estado;
+}
+
+std::string Cliente::get_estado() {
+	return estado;
+}
+
+void Cliente::set_cep(std::string _cep) {
+	cep = _cep;
+}
+
+std::string Cliente::get_cep() {
+	return cep;
+}
+
+void Cliente::print() {
+	std::cout << "  Nome: " << nome << std::endl
+			  << "  Endereço: " << endereco << std::endl
+			  << "  Cidade: " << cidade << std::endl
+			  << "  Estado: " << estado << std::endl
+			  << "  CEP: " << cep << std::endl;
 }
--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,46 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
 class Cliente{
 
-	public:
+	public:	
+		Cliente(
+			std::string _nome,
+			std::string _endereco,
+			std::string _cidade,
+			std::string _estado,
+			std::string _cep
+		) : nome(_nome),
+			endereco(_endereco),
+			cidade(_cidade),
+			estado(estado),
+			cep(_cep) {}
 
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+		void print();
+
+		void set_nome(std::string _nome);
+		std::string get_nome();
+
+		void set_endereco(std::string _endereco);
+		std::string get_endereco();
+
+		void set_cidade(std::string _cidade);
+		std::string get_cidade();
+
+		void set_estado(std::string _estado);
+		std::string get_estado();
+
+		void set_cep(std::string _cep);
+		std::string get_cep();
+
+	
+	private:
+		std::string nome;
+		std::string endereco;
+		std::string cidade;
+		std::string estado;
+		std::string cep;
 };
 
 #endif
