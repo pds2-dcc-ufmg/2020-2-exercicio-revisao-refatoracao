cdfecb276d8a33718c6b6957b8a232c430322de1
dmrohrs <77017219+dmrohrs@users.noreply.github.com>
2021-03-04 09:58:20 -0300

    ORGANIZAÇÃO GERAL
    Organização das classes em um único módulo, padronização das variáveis, abstração de métodos, virtualização de métodos, criação de construtores para as classes, adição de getters.

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -3,8 +3,7 @@
 #define Encomenda_H
 
 #include "Cliente.hpp"
-
-const int taxa = 0.25;
+#include <iostream>
 
 namespace Compra
 {
@@ -12,33 +11,54 @@ namespace Compra
     {
 
     public:
+        Encomenda(double peso, double custo, Cliente remetente, Cliente destinatario) : PESO(peso), CUSTOkg(custo), REMETENTE(remetente), DESTINATARIO(destinatario){};
+        Encomenda(){};
+        std::string getTipo();
         double getPeso();
         double getCustokg();
         virtual double calcula() = 0;
         virtual void print();
 
-    private:
+    protected:
+        const double TAXA = 0.25;
+        std::string TIPO = "voce eh muito burro";
         double PESO = 0.0;
         double CUSTOkg = 0.0;
         double T = 0.0;
-        Cliente remetente;
-        Cliente destinatario;
+        Cliente REMETENTE;
+        Cliente DESTINATARIO;
     };
 
     class EncomendaNormal : public Encomenda
     {
-
     public:
+        EncomendaNormal(double peso, double custo, Cliente remetente, Cliente destinatario)
+        {
+            this->TIPO = "normal";
+            this->PESO = peso;
+            this->CUSTOkg = custo;
+            this->REMETENTE = remetente;
+            this->DESTINATARIO = destinatario;
+        }
+        EncomendaNormal(){};
         double calcula();
-        void print() overrride;
+        void print() override;
     };
 
     class EncomendaRelampago : public Encomenda
     {
-
     public:
+        EncomendaRelampago(double peso, double custo, Cliente remetente, Cliente destinatario)
+        {
+            this->TIPO = "relampago";
+            this->PESO = peso;
+            this->CUSTOkg = custo;
+            this->REMETENTE = remetente;
+            this->DESTINATARIO = destinatario;
+        }
+        EncomendaRelampago(){};
         double calcula();
-        void print();
+        void print() override;
     };
 }
 #endif
