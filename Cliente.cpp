597aef5e40c010cbbe704c3c0369550d404add38
MasterA10 <80067897+MasterA10@users.noreply.github.com>
2021-03-04 18:37:48 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,12 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+void Cliente::imprimir(){//metodo para imprimir os dados do cliente
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+	std::cout << "  Nome: " << NOME << std::endl
+		 << "  Endereço: " << endereco << std::endl
+		 << "  Cidade: " << CIDADE << std::endl
+		 << "  Estado: " << ESTADO << std::endl
+		 << "  CEP: " << cep << std::endl;
 
 }
