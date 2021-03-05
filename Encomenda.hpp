fef89e05d3bcd17af195ce19535d70b3afca839c
Isis <isisferreiracarvalho@yahoo.com.br>
2021-03-04 22:22:37 -0300

    Refatoração do código dado

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -2,24 +2,46 @@
 #define Encomenda_H
 
 #include "Cliente.hpp"
-using namespace std;
 
 class Encomenda{
 
+	protected:
+
+		double _peso = 0.0;
+		double _custo_por_kg = 0.0;
+		double _custo_total = 0.0;
+		Cliente _remetente;
+		Cliente _destinatario;
+
 	public:
 
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
-			
-		void print(){
+		static constexpr double taxa_relampago = 0.25;
 
-			std::cout << "[Remetente]" << endl;
-			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
+		void print_remet_dest(){
+
+			std::cout << "[Remetente]" << std::endl;
+			this->_remetente.print_cliente();
+			std::cout << "[Destinatário]" << std::endl;
+			this->_destinatario.print_cliente();
+		}
+
+		virtual double calcula_custo_total(){
+
+			double custo_total = _peso * _custo_por_kg;
+
+			this->_custo_total = custo_total;
+
+			return custo_total;
+		}
+
+		void print_enc_normal(){
+
+			Encomenda::print_remet_dest();
+			std::cout << "[Encomenda Normal]" << std::endl;
+			std::cout << "  Peso: " << _peso << std::endl;
+			std::cout << "  Custo por kg: " << _custo_por_kg << std::endl;
+			std::cout << "  Custo total: " << _custo_total << std::endl;
+
 		}
 
 };
