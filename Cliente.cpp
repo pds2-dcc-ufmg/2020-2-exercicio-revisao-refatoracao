0823611d5601405c941089d97993ea0ccc883fa3
MasterA10 <80067897+MasterA10@users.noreply.github.com>
2021-03-05 01:21:56 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -9,5 +9,38 @@ void Cliente::imprimir(){//metodo para imprimir os dados do cliente
 		 << "  Cidade: " << CIDADE << std::endl
 		 << "  Estado: " << ESTADO << std::endl
 		 << "  CEP: " << cep << std::endl;
-
 }
+	//get retorna o valor desejado
+	//set altera o valor desejado
+	
+	std::string Cliente::getNome(){
+		return NOME;
+	}
+	std::string Cliente::getEndereco(){
+		return endereco;
+	}
+	std::string Cliente::getCidade(){
+		return CIDADE;
+	}
+	std::string Cliente::getEstado(){
+		return ESTADO;
+	}
+	std::string Cliente::getCep(){
+		return cep;
+	}
+
+	void Cliente::setNome(std::string aux){
+		this->NOME = aux;
+	}
+	void Cliente::setEndereco(std::string aux){
+		this->endereco = aux;
+	}
+	void Cliente::setCidade(std::string aux){
+		this->CIDADE = aux;
+	}
+	void Cliente::setEstado(std::string aux){
+		this->ESTADO = aux;
+	}
+	void Cliente::setCep(std::string aux){
+		this->cep = aux;
+	}
