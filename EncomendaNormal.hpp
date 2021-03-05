d3968ba06af03520ddae984a983cc550470c9f5f
Erlan-sousa <80107945+Erlan-sousa@users.noreply.github.com>
2021-03-05 12:47:55 -0300

    NovaEncomendaNormal

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -8,21 +8,23 @@ using namespace std;
 class EncomendaNormal: public Encomenda{
 
 	public:
-		
-	double calcula(){
-
-		double x = PESO * CUSTOkg;
-
-		return x;
+	
+	// constroi uma encomenda normal e atribui os valores
+	EncomendaNormal(double peso; double custo_kg; Cliente remetente; Cliente destinatario){
+		this-> _peso = peso;
+		this-> _custo_kg = custo_kg;
+		this-> _rementente = remetente;
+		this-> _destinatario = destinatario	
 	}
+	
+	//imprime os dados da encomenda normal e dos clientes envolvidos
+	void printEncomendaNormal(){
 
-	void print(){
-
-		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
+		Encomenda::printRemetenteDestinatario();
+		std::cout << "[Encomenda Normal]" << std::endl;
+		std::cout << " Peso: " << this-> _peso << std::endl;
+		std::cout << " Custo por kg: " << this-> _custo_kg << std::endl;
+		std::cout << " Custo total: " << this-> _custo_total << std::endl;
 
 	}
 
