be050cc35ff1d4f99b5e6b98070d8ea65c459594
Erlan-sousa <80107945+Erlan-sousa@users.noreply.github.com>
2021-03-05 12:47:25 -0300

    NovoCliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,19 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+Cliente::Cliente(std::string nome; std::string endereco; std::string cidade; std::string estado; std::string cep){
+	this-> _nome = nome;
+	this-> _endereco = endereco;
+	this-> _cidade = cidade;
+	this-> _estado = estado;
+	this-> _cep = cep;
+}
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+void Cliente::printCliente(){
 
+	std::cout << " Nome: " << this-> _nome << std::endl;
+	std::cout << " Endereço: " << this-> _endereco << std::endl;
+	std::cout << " Cidade: " << this-> _cidade << std::endl;
+	std::cout << " Estado: " << this-> _estado << std::endl;
+	std::cout << " CEP: " << this->_cep << std::endl;
 }
