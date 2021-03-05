01f0515121d44304e754823fbb425e1f64c420f0
Erlan-sousa <80107945+Erlan-sousa@users.noreply.github.com>
2021-03-05 12:46:08 -0300

    NovoCliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,19 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
 class Cliente{
 
 	public:
-
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+		Cliente(); // Construtor de clientes
+		Cliente(std::string nome; std::string endereco; std::string cidade; std::string estado; std::string cep); // Cadastro de clientes
+		void printCliente(); // imprime na tela os dados de um cliente cadastrado
+	private:
+		std::string _nome;
+		std::string _endereco;
+		std::string _cidade;
+		std::string _estado;
+		std::string _cep;
 };
 
 #endif
