446a8df2b79af69c75de718125078d3faf53e0d7
André Versiani <lumini2@hotmail.com.br>
2021-03-05 20:16:32 -0300

    refactor: move the implementation of EncomendaNormal, EncomendaRelampago classes to a another file

---
+++ src/encomenda/encomendaRelampago/EncomendaRelampago.cpp
@@ -0,0 +1,19 @@
+#include "../Encomenda.hpp"
+#include "../../cliente/Cliente.hpp"
+#include "EncomendaRelampago.hpp"
+#include "iostream"
+
+double EncomendaRelampago::calcula() {
+  double result = this->getPeso() * this->getCustoKG();
+	result += result * 0.25;
+	return result;
+}
+
+void EncomendaRelampago::print(){
+	Encomenda::print();
+	std::cout << "[Encomenda Relâmpago]" << endl;
+	std::cout << "  Peso: " << this->getPeso() << endl
+	<< "  Custo por kg: " << this->getCustoKG() << endl
+	<< "  Taxa adicional: " << 0.25 << endl
+	<< "  Custo total: " << this->getT() << endl;
+}
