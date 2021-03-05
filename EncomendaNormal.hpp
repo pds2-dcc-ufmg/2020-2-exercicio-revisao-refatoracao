7e224e863de9baae4e1d9311bb33eccba085d129
GabrielJunqueira <64487574+GabrielJunqueira@users.noreply.github.com>
2021-03-05 11:03:47 -0300

    Update EncomendaNormal.hpp
    Updates em EncomendaNormal.hpp:
    
    Criação um novo construtor, para receber o peso e custo por parâmetro.
    A classe passou a herdar o método "calculaEntrega" de sua superclasse Encomenda.hpp com override.
    Criação de comentários para detalhar o que cada método faz.
    Ajustes de identação e espaçamento para melhor visualização e entendimento.

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -5,25 +5,35 @@
 #include "Cliente.hpp"
 using namespace std;
 
-class EncomendaNormal: public Encomenda{
+class EncomendaNormal: public Encomenda {
 
-	public:
-		
-	double calcula(){
+public:
 
-		double x = PESO * CUSTOkg;
-
-		return x;
+	EncomendaNormal() {
 	}
 
-	void print(){
+	//construtor
+	EncomendaNormal(double _peso, double _custokg) {
+		this->peso = _peso;
+		this->custoKg = _custokg;
+	}
+
+	//método que calcula o valor de Entregas Normais
+	double calculaEntrega() override {
+
+		double valorEntregaNormal = peso * custoKg;
+
+		return valorEntregaNormal;
+	}
+
+	//método que imprime dados de Entregas Normais
+	void print() {
 
 		Encomenda::print();
 		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
-
+		std::cout << "  Peso: " << peso   << endl
+				  << "  Custo por kg: "   << custoKg << endl
+				  << "  Custo total: "    << custoTotal << endl;
 	}
 
 };
