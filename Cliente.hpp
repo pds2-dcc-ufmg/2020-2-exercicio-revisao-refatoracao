cddd15acefed13a98c55d6c12706238372f8bfe0
MasterA10 <80067897+MasterA10@users.noreply.github.com>
2021-03-05 01:22:22 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -9,13 +9,26 @@ class print{
 
 class Cliente: public print{
 
-	public:
-
+	protected:
 		std::string NOME;
 		std::string endereco;
 		std::string CIDADE;
 		std::string ESTADO;
 		std::string cep;
+
+	public:
+		std::string getNome();
+		std::string getEndereco();
+		std::string getCidade();
+		std::string getEstado();
+		std::string getCep();
+		
+		void setNome(std::string aux);
+		void setEndereco(std::string aux);
+		void setCidade(std::string aux);
+		void setEstado(std::string aux);
+		void setCep(std::string aux);
+
 
 		void imprimir() override;
 };
