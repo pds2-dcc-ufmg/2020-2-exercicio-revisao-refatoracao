a919f91a3ce60c4e795320da6d66aee5ef898387
CaioTeraoka <80132097+CaioTeraoka@users.noreply.github.com>
2021-03-05 20:01:52 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -7,12 +7,6 @@ using namespace std;
 class Encomenda{
 
 	public:
-
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
 			
 		void print(){
 
@@ -20,7 +14,36 @@ class Encomenda{
 			remetente.print();
 			std::cout << "[Destinatário]" << endl;
 			dest.print();
+			if(classe == "normal"){
+			std::cout << "[Encomenda Normal]" << endl;
+		std::cout << "  Peso: " << PESO << endl
+			<< "  Custo por kg: " << CUSTOkg << endl
+			<< "  Custo total: " << T << endl;
+			}
+			if(classe == "relampago"){
+			std::cout << "[Encomenda Relâmpago]" << endl;
+			std::cout << "  Peso: " << PESO << endl
+				<< "  Custo por kg: " << CUSTOkg << endl
+				<< "  Taxa adicional: " << 0.25 << endl
+				<< "  Custo total: " << T << endl;
+			}		
 		}
+		double calcula(){
+		valor = PESO * CUSTOkg;
+		if(classe == "relampago"){
+		valor += valor * 0.25;}
+		return valor;
+		}
+	
+	private:
+		double PESO = 0.0;
+		double CUSTOkg = 0.0;
+		double T = 0.0;
+		Cliente remetente;
+		Cliente dest;
+		string classe;
+		double valor = 0;
+	
 
 };
 
