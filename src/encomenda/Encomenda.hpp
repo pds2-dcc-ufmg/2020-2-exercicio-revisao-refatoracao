a8d7163c4a6add3671efc40d2ac1e55d582bd9af
André Versiani <lumini2@hotmail.com.br>
2021-03-05 20:14:54 -0300

    refactor: add getT and setT methods in Encomenda class header

--- src/encomenda/Encomenda.hpp
+++ src/encomenda/Encomenda.hpp
@@ -5,27 +5,29 @@
 using namespace std;
 
 class Encomenda {
-  
-  Encomenda();
-  Encomenda(double peso, double custoKg, Cliente remetente, Cliente dest);
 	
   public:
+    Encomenda();
+    Encomenda(double peso, double custoKg, Cliente remetente, Cliente dest);
+    
     void print();
     
     void setPeso(double peso);
     void setCustoKG(double custo);
     void setRemetente(Cliente remetente);
     void setDest(Cliente dest);
+    void setT(double t);
 
     double getPeso();
     double getCustoKG();
+    double getT();
     Cliente getRemetente();
     Cliente getDest();
 
   private:
     double peso;
 		double custoKg;
-		double t;
+		double t = 0.0;
 		Cliente remetente;
 		Cliente dest;
 };
