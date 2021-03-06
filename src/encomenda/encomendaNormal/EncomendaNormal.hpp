3bbcad8354d06a72474d537c0105a6b3ec7125fb
André Versiani <lumini2@hotmail.com.br>
2021-03-05 21:04:13 -0300

    refactor: remove the wrong indentation

--- src/encomenda/encomendaNormal/EncomendaNormal.hpp
+++ src/encomenda/encomendaNormal/EncomendaNormal.hpp
@@ -8,13 +8,11 @@
 using namespace std;
 
 class EncomendaNormal: public Encomenda {
-
-	public:
+  public:
     EncomendaNormal():Encomenda(){};
     EncomendaNormal(double peso, double custoKg, Cliente remetente, Cliente dest):Encomenda(peso, custoKg, remetente, dest){};
     double calcula();
     void print();
-
 };
 
 #endif
