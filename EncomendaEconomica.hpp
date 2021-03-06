82cafb0d94abe5ec6dc1468ac50afa8318d2389b
Henrique Perim <henrique.d.perim@gmail.com>
2021-03-06 21:16:22 +0000

    Henrique Daré Perim 2019042554 Adicionei um arquivo, bem como uma subclasse "EcomendaEconomica"; Retirei o númeoro mágico da taxa, atribuindo este a uma variáve. Deixei os nomes com uma caixa padrão e os tornei de fácil entendimento.

--- EncomendaEconomica.hpp
+++ EncomendaEconomica.hpp
@@ -1,30 +1,27 @@
-#ifndef EncomendaRelampago_H
-#define EncomendaRelampago_H
+#ifndef EncomendaEconomica_H
+#define EncomendaEconomica_H
 
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
 using namespace std;
 
-class EncomendaRelampago: public Encomenda{
+class EncomendaEconomica: public Encomenda{
 
 	public:
-  
-    double taxaEncomendaRelampago = 0.25;
-
+    double descontoEncomendaEconomica = 0.2;
 		double calcula(){
 			double x = peso * custoKg;
-			x += x * taxaEncomendaRelampago;
-			
+			x -= x * descontoEncomendaEconomica;
 			return x;
 		}
 
 		void print(){
 
 			Encomenda::print();
-			std::cout << "[Encomenda Relâmpago]" << endl;
+			std::cout << "[Encomenda Economica]" << endl;
 			std::cout << "  Peso: " << peso << endl
 				<< "  Custo por kg: " << custoKg << endl
-				<< "  Taxa adicional: " << taxaEncomendaRelampago << endl
+				<< "  Desconto Encomenda Economica: " << descontoEncomendaEconomica << endl
 				<< "  Custo total: " << custoTotal << endl;
 
 		}
