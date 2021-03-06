94c394ca4d42f076765c72fe98de736198b6b2aa
André Versiani <lumini2@hotmail.com.br>
2021-03-06 00:35:50 -0300

    refactor: move makeEncomenda and makeCalculo to a factory method

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <string>
 #include "./encomenda/Encomenda.hpp"
 #include "./encomenda/encomendaNormal/EncomendaNormal.hpp"
 #include "./encomenda/encomendaRelampago/EncomendaRelampago.hpp"
 #include "./cliente/Cliente.hpp"
 using namespace std;
 
+template <class Template> Template makeEncomenda(int *count, double peso, double custoKg, Cliente remetente, Cliente dest) {
+  (*count)++;
+  return Template(peso, custoKg, remetente, dest);
+}
+
+template <class Template> void makeCalculo(Template encomenda, double *ptr) {
+  encomenda.calcula();
+	encomenda.print();
+	(*ptr) += encomenda.calcula();
+	std::cout << endl;
+}
+
 int main(){
-	int quantN = 0;
+	
+  int quantN = 0;
 	int quantR = 0;
+  int *quantNptr;
+  int *quantRptr;
+  quantNptr = &quantN;
+  quantRptr = &quantR;
+
+  double custN = 0;
+	double custR = 0;
+  double *custNptr;
+  double *custRptr;
+  custNptr = &custN;
+  custRptr = &custR;
+
 	Cliente cli0 = Cliente("Thales", "Rua dos Pré-Socráticos", "Miletus", "Ionia", "548 a.C.");
 	Cliente cli1 = Cliente("Aristóteles", "Avenida do Meio-Termo", "Atenas", "Ática", "384 a.C.");
 	Cliente cli2 = Cliente("Platão", "Praça das Formas", "Atenas", "Ática", "348 a.C");
@@ -16,67 +42,26 @@ int main(){
   Cliente cli5 = Cliente("Parmênides", "Rua do Não Ser", "Eleia", "Magna Grécia", "460 a.C.");
   Cliente cli6 = Cliente("Empédocles", "Rua dos Quatro Elementos", "Agrigento", "Sicília", "495 a.C.");
 	Cliente cli7 = Cliente("Anaxágoras", "Avenida da Mente Cósmica", "Clazômenas", "Jónia", "499 a.C.");
-	EncomendaNormal enc0 = EncomendaNormal(5, 12, cli0, cli1);
-	quantN++;
-	EncomendaNormal enc1 = EncomendaNormal(10, 12, cli1, cli2);
-	quantN++;
-	EncomendaNormal enc2 = EncomendaNormal(7, 12, cli2, cli3);
-	quantN++;
-  EncomendaNormal enc3 = EncomendaNormal(2, 12, cli3, cli4);
-	quantN++;
-	EncomendaNormal enc4 = EncomendaNormal(3, 12, cli4, cli5);
-	quantN++;
-	EncomendaRelampago enc5 = EncomendaRelampago(13, 18, cli5, cli6);
-	quantR++;
-	EncomendaRelampago enc6 = EncomendaRelampago(6, 18, cli6, cli7);
-	quantR++;
-	EncomendaRelampago enc7 = EncomendaRelampago(8, 18, cli7, cli0);
-  quantR++;
+	
+  EncomendaNormal enc0 = makeEncomenda<EncomendaNormal>(quantNptr, 5, 12, cli0, cli1);
+	EncomendaNormal enc1 = makeEncomenda<EncomendaNormal>(quantNptr, 10, 12, cli1, cli2);
+	EncomendaNormal enc2 = makeEncomenda<EncomendaNormal>(quantNptr, 7, 12, cli2, cli3);
+  EncomendaNormal enc3 = makeEncomenda<EncomendaNormal>(quantNptr, 2, 12, cli3, cli4);
+	EncomendaNormal enc4 = makeEncomenda<EncomendaNormal>(quantNptr, 3, 12, cli4, cli5);
+	EncomendaRelampago enc5 = makeEncomenda<EncomendaRelampago>(quantRptr, 13, 18, cli5, cli6);
+	EncomendaRelampago enc6 = makeEncomenda<EncomendaRelampago>(quantRptr, 6, 18, cli6, cli7);
+	EncomendaRelampago enc7 = makeEncomenda<EncomendaRelampago>(quantRptr, 8, 18, cli7, cli0);
 	
 	std::cout << "\n>> Relatório de encomendas <<" << endl;
-
-	double custN = 0;
-	double custR = 0;
-
-	enc0.calcula();
-	enc0.print();
-	custN += enc0.calcula();
-	std::cout << endl;
-
-	enc1.calcula();
-	enc1.print();
-	custN += enc1.calcula();
-	std::cout << endl;
-
-	enc2.calcula();
-	enc2.print();
-	custN += enc2.calcula();
-	std::cout << endl;
-
-	enc3.calcula();
-	enc3.print();
-	custN += enc3.calcula();
-	std::cout << endl;
-
-	enc4.calcula();
-	enc4.print();
-	custN += enc4.calcula();
-	std::cout << endl;
-
-	enc5.calcula();
-	enc5.print();
-	custR += enc5.calcula();
-	std::cout << endl;
-
-	enc6.calcula();
-	enc6.print();
-	custR += enc6.calcula();
-	std::cout << endl;
-
-	enc7.calcula();
-	enc7.print();
-	custR += enc7.calcula();
-	std::cout << endl;
+  
+  makeCalculo<EncomendaNormal>(enc0, custNptr);
+  makeCalculo<EncomendaNormal>(enc1, custNptr);
+  makeCalculo<EncomendaNormal>(enc2, custNptr);
+  makeCalculo<EncomendaNormal>(enc3, custNptr);
+  makeCalculo<EncomendaNormal>(enc4, custNptr);
+  makeCalculo<EncomendaRelampago>(enc5, custRptr);
+  makeCalculo<EncomendaRelampago>(enc6, custRptr);
+  makeCalculo<EncomendaRelampago>(enc7, custRptr);
 	
 	std::cout << "\n>> Encomendas Normais <<" 
 		 << "\nQuantidade: " << quantN
@@ -86,5 +71,5 @@ int main(){
 		 << "\nQuantidade: " << quantR
 		 << "\nValor Total: " << custR
 		 << endl;
-	
 }
+
