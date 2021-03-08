2327c46cf9c37c3ed219748daef21fe06e98b0d9
Philippe <lipeph99@gmail.com>
2021-03-08 19:00:23 -0300

    Update Encomenda.hpp
    Nome das variáveis alterado para nomes padronizados e mais legíveis
    Comentários adicionados para explicar as funções
    Criado construtor de Encomenda usando parametros

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -8,19 +8,39 @@ class Encomenda{
 
 	public:
 
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
+		double peso = 0.0;
+		double custoKg = 0.0;
+		double custoTotal = 0.0;
 		Cliente remetente;
 		Cliente dest;
-			
+		
+		/**
+		* Printa na tela as informações da encomenda
+		*/
 		void print(){
-
 			std::cout << "[Remetente]" << endl;
 			remetente.print();
 			std::cout << "[Destinatário]" << endl;
 			dest.print();
 		}
+
+		Encomenda();
+		
+		/**
+		* Construtor de encomenda normal usando parametros.
+		*/
+		Encomenda(double ePeso, double eCUSTOkg, Cliente eRemetente, Cliente eDest){
+			this->peso = ePeso;
+			this->custoKg = eCUSTOkg;
+			this->remetente = eRemetente;
+			this->dest = eDest;
+			this->custoTotal = 0.0;
+		}
+
+		/**
+		* Função virtual que precisa ser reescrita pelos outros métodos
+		*/
+		virtual double CalcularTotal()=0;
 
 };
 
