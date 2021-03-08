0081affd5767a777a22dc0f642893928008cf3a8
izaias1809 <80263119+izaias1809@users.noreply.github.com>
2021-03-08 11:00:53 -0300

    Update EncomendaRelampago.hpp

--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -9,25 +9,9 @@ class EncomendaRelampago: public Encomenda{
 
 	public:
 
-		double calcula(){
+		double calcula();
 
-			double x = PESO * CUSTOkg;
-			x += x * 0.25;
-			
-			return x;
-		}
-
-		void print(){
-
-			Encomenda::print();
-			std::cout << "[Encomenda Relâmpago]" << endl;
-			std::cout << "  Peso: " << PESO << endl
-				<< "  Custo por kg: " << CUSTOkg << endl
-				<< "  Taxa adicional: " << 0.25 << endl
-				<< "  Custo total: " << T << endl;
-
-		}
-
+		void print(); 
 };
 
 #endif
