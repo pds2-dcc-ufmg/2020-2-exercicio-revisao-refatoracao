2359b176334eafb2692178cad7ac85401caca4bc
izaias1809 <80263119+izaias1809@users.noreply.github.com>
2021-03-08 11:05:30 -0300

    Create EncomendaNormal.cpp

---
+++ EncomendaNormal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <EncomendaNormal.hpp>
+
+double calcula()
+{
+		double x = weight * cost_kg;
+		return x;
+	}
+
+	void print()
+  {
+		Encomenda::print();
+		std::cout << "[Encomenda Normal]" << endl;
+		std::cout << "  Peso: " << weight << endl
+			<< "  Custo por kg: " << cost_kg << endl
+			<< "  Custo total: " << T << endl;
+	}
