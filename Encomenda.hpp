0bdcef357e60ea2b80afd35a5470b395a0ca2aa5
diegoamaral06 <79801345+diegoamaral06@users.noreply.github.com>
2021-03-08 11:03:17 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -2,26 +2,35 @@
 #define Encomenda_H
 
 #include "Cliente.hpp"
-using namespace std;
 
 class Encomenda{
 
-	public:
+  protected:
+    double peso = 0.0;
+    double custo_kg = 0.0;
+    double T = 0.0;
+    Cliente remetente;
+    Cliente destinatario;
 
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
+  
+   public:
+
+    Encomenda(double mass, double cost, Cliente rement, Cliente dest);
+
+    double get_peso(){return this->peso;}
+    double get_custo_kg(){return this->custo_kg;}
+    double get_T(){return this->T;}
+    Cliente get_remetente(){return this->remetente;}
+    Cliente get_destinatario(){return this->destinatario;}
+
+    void set_peso(double weight){this->peso = weight;}
+    void set_custo_kg(double cost){this->custo_kg = cost;}
+    void set_T(double t){this->T = t;}
+    void set_remetente(Cliente rement){this->remetente = rement;}
+    void set_destinatario(Cliente dest){this->destinatario = dest;}
 			
-		void print(){
-
-			std::cout << "[Remetente]" << endl;
-			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
-		}
-
+    virtual void print();
+    virtual double calcula();
 };
 
 #endif
