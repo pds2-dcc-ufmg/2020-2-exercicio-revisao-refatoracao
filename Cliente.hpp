65fbac8c485c4109af3895f85b673ebe35000804
diegoamaral06 <79801345+diegoamaral06@users.noreply.github.com>
2021-03-08 11:01:20 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,42 @@
 #define CLIENTE_H
 
 #include <string>
+
 using namespace std;
 
 class Cliente{
 
 	public:
 
-		string NOME;
+    Cliente(){};
+    Cliente(string, string, string, string, string);
+
+    void print();
+
+    string get_nome(){return this->nome;}
+    void set_nome(string name){this->nome = name;}
+    
+    string get_endereco(){return this->endereco;}
+    void set_endereco(string index){this->endereco = index;}
+
+    string get_cidade(){return this->cidade;}
+    void set_cidade(string city){this->cidade = city;}
+
+    string get_estado(){return this->estado;}
+    void set_estado(string state){this->estado = state;}
+
+    string get_CEP(){return this->CEP;}
+    void set_CEP(string cep){this->CEP = cep;}
+
+  
+  private:
+
+    string nome;
 		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+		string cidade;
+		string estado;
+		string CEP;
+
 };
 
 #endif
