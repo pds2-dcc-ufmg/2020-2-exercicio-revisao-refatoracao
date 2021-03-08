7cc9a56b1e906795ce77e3e2b839357a4d5f6497
IgVieir <66919484+IgVieir@users.noreply.github.com>
2021-03-08 18:03:40 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,6 +2,17 @@
 #include <string>
 #include "Cliente.hpp"
 
+//Construtor da classe Cliente 
+Cliente::Cliente(string nome,string endereco,string cidade,string estado,string cep){
+    _nome = nome;
+    _endereco = endereco;
+    _cidade = cidade;
+    _estado = estado;
+    _cep = cep;
+
+}
+
+//Função print escreve na tela todos os dados armazenados na classe Cliente
 void Cliente::print(){
 
 	std::cout << "  Nome: " << _nome << endl
@@ -9,5 +20,4 @@ void Cliente::print(){
 		 << "  Cidade: " << _cidade << endl
 		 << "  Estado: " << _estado << endl
 		 << "  CEP: " << _cep << endl;
-
 }
