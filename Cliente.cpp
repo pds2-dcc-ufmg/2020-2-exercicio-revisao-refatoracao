38bae2000fa5e865c6f771d0b4e6ace004d0ab6f
Jarley <jarleyjunior@gmail.com>
2021-03-08 16:40:27 -0300

    Implementei todos os metodos criados (gets e sets)

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,7 +2,47 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+std::string Cliente::get_nome() {
+	return this->nome;
+}
+
+std::string Cliente::get_endereco() {
+	return this->endereco;
+}
+
+std::string Cliente::get_cidade() {
+	return this->cidade;
+}
+
+std::string Cliente::get_estado() {
+	return this->estado;
+}
+
+std::string Cliente::get_cep() {
+	return this->cep;
+}
+
+Cliente::set_nome( std::string nome ) {
+	this->nome = nome;
+}
+
+Cliente::set_endereco( std::string endereco ) {
+	this->endereco = endereco;
+}
+
+Cliente::set_cidade( std::string cidade ) {
+	this->cidade = cidade;
+}
+
+Cliente::set_estado( std::string estado ) {
+	this->estado = estado;
+}
+
+Cliente::set_cep( std::string cep ) {
+	this->cep = cep;
+}
+
+void Cliente::print() {
 
 	std::cout << "  Nome: " << nome << std::endl
 		 << "  Endereço: " << endereco << std::endl
--- Encomenda.hpp
+++ Encomenda.hpp
@@ -7,7 +7,7 @@ class Encomenda{
 
 	public:
 	
-		void print(){
+		void print() {
 
 			std::cout << "[Remetente]" << std::endl;
 			remetente.print();
@@ -15,17 +15,17 @@ class Encomenda{
 			dest.print();
 		}
 
-		double get_peso();
-		double get_custoKG();
-		double get_T();
-		cliente get_remetente();
-		cliente get_dest();
+		double get_peso() { return this->peso };
+		double get_custoKG() { return this->custoKG };
+		double get_T() { return this->T };
+		cliente get_remetente() { return this->remetente };
+		cliente get_dest() { return this->dest };
 
-		set_peso( double peso );
-		set_custoKG( double custoKG );
-		set_T( double T );
-		set_remetente( Cliente remetente );
-		set_dest( Cliente dest );
+		set_peso( double peso ) { this->peso = peso };
+		set_custoKG( double custoKG ) { this->custoKG = custoKG };
+		set_T( double T ) { this->T = T };
+		set_remetente( Cliente remetente ) { this->remetente = remetente };
+		set_dest( Cliente dest ) { this->dest = dest };
 
 	protected:
 
