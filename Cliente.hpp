510316a552295a37a5215c7479a9942401f79df5
IgVieir <66919484+IgVieir@users.noreply.github.com>
2021-03-08 18:04:52 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,15 +7,18 @@ using namespace std;
 class Cliente{
 
 	public:
+	    Cliente(string nome="0",string endereco="0",string cidade="0",string estado="0",string cep="0");
 
-		string _nome;
+		void print(); // imprime na tela os dados de um cliente cadastrado
+
+    protected:
+        string _nome;
 		string _endereco;
 		string _cidade;
 		string _estado;
 		string _cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+
 };
 
 #endif
+
