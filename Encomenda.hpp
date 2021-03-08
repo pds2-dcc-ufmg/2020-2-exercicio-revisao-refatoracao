f4bce7bd341c8b2d2704b058a5af3774bafc4b17
Gabriel-772 <80278001+Gabriel-772@users.noreply.github.com>
2021-03-08 15:04:35 -0300

    2020070620 Gabriel Soares Barbosa
    -Adicionei camadas de privacidade e método construtor nas classes
    -Adicionei o valor Total
    -Criei funções get e set para as classes ciente e encomendas 
    -Criei funções para facilitar o processo dos relatórios
    -criei um vetor para guardar as encomendas criadas e ficar mais fácil de printar os relatórios no main

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -7,21 +7,33 @@ using namespace std;
 class Encomenda{
 
 	public:
-
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
 			
 		void print(){
 
 			std::cout << "[Remetente]" << endl;
-			remetente.print();
+			remetente->print();
 			std::cout << "[Destinatário]" << endl;
-			dest.print();
+			dest->print();
 		}
+		void set_peso(double p){this->PESO= p;}
+		double get_Peso(){return this->PESO;}
 
+		void set_Custokg(double c){this->CUSTOkg= c;}
+		double get_Custokg(){return this->CUSTOkg;}
+
+		void set_Remetente(Cliente *r){this->remetente= r;}
+		Cliente* get_Remetente(){return this->remetente;}
+
+		void set_Destinatario(Cliente *d){this->dest= d;}
+		Cliente* get_Destinatario(){return this->dest;}
+	;
+	protected:
+		double PESO = 0.0;
+		double CUSTOkg = 0.0;
+		double T = 0.0;
+		Cliente* remetente;
+		Cliente* dest;
+	;
 };
 
 #endif
--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -9,11 +9,18 @@ class EncomendaRelampago: public Encomenda{
 
 	public:
 
+		EncomendaRelampago(double P,double C,Cliente* r, Cliente* d){
+		this->PESO= P;
+		this->CUSTOkg= C;
+		this->remetente=r;
+		this->dest=d;
+	}
+
 		double calcula(){
 
 			double x = PESO * CUSTOkg;
 			x += x * 0.25;
-			
+			this->T = x;
 			return x;
 		}
 
@@ -27,6 +34,13 @@ class EncomendaRelampago: public Encomenda{
 				<< "  Custo total: " << T << endl;
 
 		}
+
+	void relatorio_enc(double *custR){
+		this->calcula();
+		this->print();
+		*custR += this->calcula();
+		std::cout << endl;
+	}
 
 };
 
