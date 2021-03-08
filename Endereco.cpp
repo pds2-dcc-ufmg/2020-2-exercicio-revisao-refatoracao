f2c0955938be98cd9037f159364381d3cad76fa7
lucas.campos <lucas.campos@camss.group>
2021-03-08 17:55:15 -0300

    Adicinando métodos que retornam os valores dos atributos de endereço

--- Endereco.cpp
+++ Endereco.cpp
@@ -2,8 +2,24 @@
 #include "Endereco.hpp"
 
 Endereco::Endereco(string logradouro, string cidade, string estado, string cep) {
-    this -> logradouro = logradouro;
-    this -> cidade = cidade;
-    this -> estado = estado;
-    this -> cep = cep;
+    this->logradouro = logradouro;
+    this->cidade = cidade;
+    this->estado = estado;
+    this->cep = cep;
+}
+
+string Endereco::getLogradouro() {
+    return this->logradouro;
+}
+
+string Endereco::getCidade() {
+    return this->cidade;
+}
+
+string Endereco::getEstado() {
+    return this->estado;
+}
+
+string Endereco::getCep() {
+    return this->cep;
 }
