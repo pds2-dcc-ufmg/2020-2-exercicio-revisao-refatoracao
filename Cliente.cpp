9bfa982789b1b0ce3153989d2d08fed22a90f2ca
thaleshqs <59453636+thaleshqs@users.noreply.github.com>
2021-03-07 23:25:27 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,55 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
+void Cliente::print() {
 
-	std::cout << "  Nome: " << NOME << endl
+	std::cout << "  Nome: " << nome << endl
 		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
+		 << "  Cidade: " << cidade << endl
+		 << "  Estado: " << estado << endl
 		 << "  CEP: " << cep << endl;
 
+}
+
+// Getters:
+string Cliente::getNome() {
+	return nome;
+}
+
+string Cliente::getEndereco() {
+	return endereco;
+}
+
+string Cliente::getCidade() {
+	return cidade;
+}
+
+string Cliente::getEstado() {
+	return estado;
+}
+
+string Cliente::getCep() {
+	return cep;
+}
+
+// Setters:
+
+void Cliente::setNome(string nome) {
+	this->nome = nome;
+}
+
+void Cliente::setEndereco(string endereco) {
+	this->nome = endereco;
+}
+
+void Cliente::setCidade(string cidade) {
+	this->cidade = cidade;
+}
+
+void Cliente::getEstado(string estado) {
+	this->estado = estado;
+}
+
+void Cliente::getCep(string cep) {
+	this->cep = cep;
 }
