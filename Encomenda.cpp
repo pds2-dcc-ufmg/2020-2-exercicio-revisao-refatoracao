70e5b5929b35c5eabadba260c84a043ca7ce5e9e
mateus-werneck <werneck.mateus@gmail.com>
2021-03-09 12:03:40 -0300

    Alterações Finais Encomenda.cpp
    Implementação metódos get(), set() e print()

--- Encomenda.cpp
+++ Encomenda.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
+using namespace std;
 
+//Get e Set para Peso
+double Encomenda::getPeso() {
+	return PESO;}
+void Encomenda::setPeso(double peso) {
+	PESO = peso;}
+
+//Get e Set para KG
+double Encomenda::getKg() {
+	return CUSTOkg;}
+void Encomenda::setKg(double kg) {
+	CUSTOkg = kg;}
+
+//Get para Total
+double Encomenda::getTotal() {
+	return T;}
+
+//Get e Set para Remetente
+Cliente Encomenda::getRemetente() {
+	return remetente;}
+void Encomenda::setRemetente(Cliente remet) {
+	remetente = remet;}
+
+//Get e Set para Destinatário
+Cliente Encomenda::getDestinatario() {
+	return dest;}
+void Encomenda::setDestinatario(Cliente destinatario) {
+	dest = destinatario;}
+
+//Método print()
 void Encomenda::print(){
 
   std::cout << "[Remetente]" << endl;
-  remetente.print();
+  getRemetente().print();
   std::cout << "[Destinatário]" << endl;
-  dest.print();
+  getDestinatario().print();
 }
