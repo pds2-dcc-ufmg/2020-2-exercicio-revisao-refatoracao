ba46a838bd306b1f21d8f3688a8ce85321e86215
isabellaufmg <79879373+isabellaufmg@users.noreply.github.com>
2021-03-09 16:53:50 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,27 @@
 #include <string>
 #include "Cliente.hpp"
 
+Cliente::Cliente(string a,string b, string c, string d, string e){
+	this->nome = a;
+	this->endereco = b;
+	this->cidade = c;
+	this->estado = d;
+	this->cep = e;
+}
+Cliente::Cliente(){
+	this->nome = "";
+	this->endereco = "";
+	this->cidade = "";
+	this->estado = "";
+	this->cep = "";
+}
+
 void Cliente::print(){
 
-	std::cout << "  Nome: " << NOME << endl
+	std::cout << "  Nome: " << nome << endl
 		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
+		 << "  Cidade: " << cidade << endl
+		 << "  Estado: " << estado << endl
 		 << "  CEP: " << cep << endl;
 
 }
