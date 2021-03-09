c86a41c4f85bf09403864503aee1a34f18e41c29
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 15:36:48 -0300

    Update EncomendaRelampago.hpp
    separei a implementação dos métodos para um .cpp

--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -9,25 +9,9 @@ class EncomendaRelampago: public Encomenda{
 
 	public:
 
-		double calcula(){
-
-			double x = _peso * _custoKg;
-			x += x * 0.25;
-			
-			return x;
-		}
-
-		void print(){
-
-			Encomenda::print();
-			std::cout << "[Encomenda Relâmpago]" << endl;
-			std::cout << "  Peso: " << _peso << endl
-				<< "  Custo por kg: " << _custoKg << endl
-				<< "  Taxa adicional: " << 0.25 << endl
-				<< "  Custo total: " << _total << endl;
-
-		}
-
+		double calcula();
+		void print();
+	
 };
 
 #endif
