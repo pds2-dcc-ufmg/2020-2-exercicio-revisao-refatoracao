1984a147e046f9a1897ebb418d8e199aa3a7874e
Augustobeybey <80132566+Augustobeybey@users.noreply.github.com>
2021-03-09 00:15:12 -0300

    Augusto César Carvalho de Souza
    Matrícula:2018018633

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Cliente.hpp"
-Cliente::Cliente(string NOME, string endereco, string CIDADE, string ESTADO, int cep){
-  this ->_nomeCliente = NOME;
-  this->_enderecoCliente = endereco;
-  this->_CIDADECliente = CIDADE;
-  this ->_ESTADOCliente = ESTADO;
-  this->_cepCliente = cep; 
-};
+
 void Cliente::print(){
 
 	std::cout << "  Nome: " << NOME << endl
