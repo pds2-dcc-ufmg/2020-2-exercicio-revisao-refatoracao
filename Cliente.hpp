65dc04887b4ad6dcf68ad4d79d7345fe5d54f622
Ari Rodrigues <72748939+arirodriguessjr@users.noreply.github.com>
2021-03-09 18:57:17 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -9,33 +9,43 @@ class Cliente{
 		void setNome(string _nome){
 		      nome = _nome;
 		}
+	
 		void setEndereco(string _endereco){
 		      endereco = _endereco;
 		}
+	
 		void setCidade(string _cidade){
 			cidade = _cidade;
 		}
+	
 		void setEstado(string _estado){
       			estado = _estado;
     		}
+	
 		void setCep(string _cep){
       			cep = _cep;
     		}
+	
     		string getNome(){
       			return this->nome;
     		}
+	
 		string getEndereco(){
 			return this->endereco;
 		}
+	
 		string getCidade(){
 			return this->cidade;
 		}
+	
 	        string getEstado(){
 	      		return this->estado;
 	    	}
+	
 	    	string getCep(){
 	      		return this->cep;
     		}
+	
 		void registro(string _nome, string _endereco, string _cidade, string _estado, string _cep){
 		      nome = _nome;
 		      endereco = _endereco;
