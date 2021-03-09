e90e7f81a6af42453147b7b03a4623eb2cceb203
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 19:47:01 -0300

    Update EncomendaNormal.hpp
    Declaração e implementação do creator de EncomendaNormal.

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -8,6 +8,7 @@ class EncomendaNormal: public Encomenda{
 
 	public:
 
+		EncomendaNormal(double peso, double custoKg, Cliente &remetente, Cliente &dest) : Encomenda(peso, custoKg, remetente, dest) {};
 		double calcula(); //Calcula o custo total da encomenda normal.
 		void print(); //Imprime o mesmo que Encomenda mais os dados de peso e custo referentes a Encomenda Normal.
 
