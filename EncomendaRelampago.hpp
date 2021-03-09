7f69e1f4c33ba76390cfac909b30a0c470545a30
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 19:48:03 -0300

    Update EncomendaRelampago.hpp
    Declaração e implementação do creator de EncomendaRelampago.

--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -10,6 +10,7 @@ class EncomendaRelampago: public Encomenda{
 
 	public:
 
+		EncomendaRelampago(double peso, double custoKg, Cliente &remetente, Cliente &dest) : Encomenda(peso, custoKg, remetente, dest) {};
 		double calcula(); //Calcula o custo total de uma Encomenda Relampago.
 		void print(); //Imprime o mesmo que Encomenda mais os dados de peso e custo referentes a Encomenda Relampago.
 
