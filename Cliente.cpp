1174a9e47de9d8f50fa871498017caf74c1224a8
thaleshqs <59453636+thaleshqs@users.noreply.github.com>
2021-03-09 16:07:20 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -3,13 +3,11 @@
 #include "Cliente.hpp"
 
 void Cliente::print() {
-
 	std::cout << "  Nome: " << nome << endl
 		 << "  Endereço: " << endereco << endl
 		 << "  Cidade: " << cidade << endl
 		 << "  Estado: " << estado << endl
 		 << "  CEP: " << cep << endl;
-
 }
 
 // Getters:
