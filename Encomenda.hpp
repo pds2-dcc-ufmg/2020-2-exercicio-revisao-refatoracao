2f4f847fdf83061708924830c58d86320261f969
Ari Rodrigues <72748939+arirodriguessjr@users.noreply.github.com>
2021-03-09 16:46:57 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -1,27 +1,48 @@
-#ifndef Encomenda_H
-#define Encomenda_H
+#ifndef ENCOMENDA_H
+#define ENCOMENDA_H
 
 #include "Cliente.hpp"
 using namespace std;
 
 class Encomenda{
-
 	public:
-
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
 		Cliente remetente;
-		Cliente dest;
-			
-		void print(){
+		Cliente destinatario;
+		
+		Encomenda(double _peso, double _custoKg, Cliente& _remetente, Cliente& _destinatario){
+		      peso = _peso;
+		      custoKg = _custoKg;
+		      remetente = _remetente;
+		      destinatario = _destinatario;
+    		}
+	
+		void setCustoTotal(double _custoTotal){
+	      		custoTotal = _custoTotal;
+	    	}
+	
+		double getPeso(){
+	      		return peso;
+	    	}
+	
+	    	double getCustoKg(){
+	      		return custoKg;
+	    	}
+	
+	    	double getCustoTotal(){
+	      		return custoTotal;
+	    	}
 
+		void print(){
 			std::cout << "[Remetente]" << endl;
 			remetente.print();
 			std::cout << "[Destinatário]" << endl;
-			dest.print();
+			destinatario.print();
 		}
-
+		
+	private:
+		double peso = 0.0;
+		double custoKg = 0.0;
+		double custoTotal = 0.0;
 };
 
 #endif
