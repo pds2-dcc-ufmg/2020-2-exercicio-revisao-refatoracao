2e54f0a7e07e9e1c03f2f09c33d2e850d215b0c5
isabellaufmg <79879373+isabellaufmg@users.noreply.github.com>
2021-03-09 17:57:20 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,137 +3,52 @@
 #include "EncomendaNormal.hpp"
 #include "EncomendaRelampago.hpp"
 #include "Cliente.hpp"
-using namespace std;
+#include <vector>
 
-int main(){
-	
-	Cliente cli0("Thales","Rua dos Pré-Socráticos","Miletus","Ionia","548 a.C.");
+int main()
+{
+	std::vector<Cliente> clientes;
+	clientes.push_back(*new Cliente("Thales", "Rua dos Pré-Socráticos", "Miletus", "Ionia", "548 a.C."));
+	clientes.push_back(*new Cliente("Aristóteles", "Avenida do Meio-Termo", "Atenas", "Ática", "384 a.C."));
+	clientes.push_back(*new Cliente("Platão", "Praça das Formas", "Atenas", "Ática", "348 a.C"));
+	clientes.push_back(*new Cliente("Sócrates", "Rua do Elenchus", "Atenas", "Ática", "399 a.C."));
+	clientes.push_back(*new Cliente("Pitágoras", "Praça dos Quadrados dos Catetos", "Samos", "Egeu", "571 a.C."));
+	clientes.push_back(*new Cliente("Parmênides", "Rua do Não Ser", "Eleia", "Magna Grécia", "460 a.C."));
+	clientes.push_back(*new Cliente("Empédocles", "Rua dos Quatro Elementos", "Agrigento", "Sicília", "495 a.C."));
+	clientes.push_back(*new Cliente("Anaxágoras", "Avenida da Mente Cósmica", "Clazômenas", "Jónia", "499 a.C."));
 
-	Cliente cli1("Aristóteles","Avenida do Meio-Termo","Atenas","Ática","384 a.C.");
-
-	Cliente cli2("Platão","Praça das Formas","Atenas","Ática","348 a.C");
-
-	Cliente cli3("Sócrates","Rua do Elenchus","Atenas","Ática","399 a.C.");
-
-	Cliente cli4("Pitágoras","Praça dos Quadrados dos Catetos","Samos","Egeu","571 a.C.");
-
-	Cliente cli5("Parmênides","Rua do Não Ser","Eleia","Magna Grécia","460 a.C.");
-
-	Cliente cli6("Empédocles","Rua dos Quatro Elementos","Agrigento","Sicília","495 a.C.");
-
-	Cliente cli7("Anaxágoras","Avenida da Mente Cósmica","Clazômenas","Jónia","499 a.C.");
-
-	int quantN = 0;
-	int quantR = 0;
-
-	EncomendaNormal enc0;
-	enc0.PESO = 5;
-	enc0.CUSTOkg = 12;
-	enc0.remetente = cli0;
-	enc0.dest = cli1;
-	quantN++;
-
-	EncomendaNormal enc1;
-	enc1.PESO = 10;
-	enc1.CUSTOkg = 12;
-	enc1.remetente = cli1;
-	enc1.dest = cli2;
-	quantN++;
-
-	EncomendaNormal enc2;
-	enc2.PESO = 7;
-	enc2.CUSTOkg = 12;
-	enc2.remetente = cli2;
-	enc2.dest = cli3;
-	quantN++;
-
-	EncomendaNormal enc3;
-	enc3.PESO = 2;
-	enc3.CUSTOkg = 12;
-	enc3.remetente = cli3;
-	enc3.dest = cli4;
-	quantN++;
-
-	EncomendaNormal enc4;
-	enc4.PESO = 3;
-	enc4.CUSTOkg = 12;
-	enc4.remetente = cli4;
-	enc4.dest = cli5;
-	quantN++;
-
-	EncomendaRelampago enc5;
-	enc5.PESO = 13;
-	enc5.CUSTOkg = 18;
-	enc5.remetente = cli5;
-	enc5.dest = cli6;
-	quantR++;
-
-	EncomendaRelampago enc6;
-	enc6.PESO = 6;
-	enc6.CUSTOkg = 18;
-	enc6.remetente = cli6;
-	enc6.dest = cli7;
-	quantR++;
-
-	EncomendaRelampago enc7;
-	enc7.PESO = 8;
-	enc7.CUSTOkg = 18;
-	enc7.remetente = cli7;
-	enc7.dest = cli0;
-	quantR++;
-	
-	std::cout << "\n>> Relatório de encomendas <<" << endl;
+	std::vector<EncomendaNormal> encomendasNormal;
+	std::vector<EncomendaRelampago> encomendasRelampago;
+	encomendasNormal.push_back(*new EncomendaNormal(5.0, 12.0, clientes[0], clientes[1]));
+	encomendasNormal.push_back(*new EncomendaNormal(10.0, 12.0, clientes[1], clientes[2]));
+	encomendasNormal.push_back(*new EncomendaNormal(7.0, 12.0, clientes[2], clientes[3]));
+	encomendasNormal.push_back(*new EncomendaNormal(2.0, 12.0, clientes[3], clientes[4]));
+	encomendasNormal.push_back(*new EncomendaNormal(3.0, 12.0, clientes[4], clientes[5]));
+	encomendasRelampago.push_back(*new EncomendaRelampago(13.0, 18.0, clientes[5], clientes[6]));
+	encomendasRelampago.push_back(*new EncomendaRelampago(6.0, 18.0, clientes[6], clientes[7]));
+	encomendasRelampago.push_back(*new EncomendaRelampago(8.0, 18.0, clientes[7], clientes[0]));
+	std::cout << "\n>> Relatório de encomendas <<" << std::endl;
 
 	double custN = 0;
 	double custR = 0;
-
-	enc0.calcula();
-	enc0.print();
-	custN += enc0.calcula();
-	std::cout << endl;
-
-	enc1.calcula();
-	enc1.print();
-	custN += enc1.calcula();
-	std::cout << endl;
-
-	enc2.calcula();
-	enc2.print();
-	custN += enc2.calcula();
-	std::cout << endl;
-
-	enc3.calcula();
-	enc3.print();
-	custN += enc3.calcula();
-	std::cout << endl;
-
-	enc4.calcula();
-	enc4.print();
-	custN += enc4.calcula();
-	std::cout << endl;
-
-	enc5.calcula();
-	enc5.print();
-	custR += enc5.calcula();
-	std::cout << endl;
-
-	enc6.calcula();
-	enc6.print();
-	custR += enc6.calcula();
-	std::cout << endl;
-
-	enc7.calcula();
-	enc7.print();
-	custR += enc7.calcula();
-	std::cout << endl;
-	
-	std::cout << "\n>> Encomendas Normais <<" 
-		 << "\nQuantidade: " << quantN
-		 << "\nValor Total: " << custN
-		 << endl
-		 << "\n>> Encomendas Relâmpago <<" 
-		 << "\nQuantidade: " << quantR
-		 << "\nValor Total: " << custR
-		 << endl;
-	
+	for (int i=0;i<5;i++){
+		encomendasNormal[i].calcula();
+		encomendasNormal[i].print();
+		custN += encomendasNormal[i].calcula();
+		std::cout << std::endl;
+	}
+	for (int i=0;i<3;i++){
+		encomendasRelampago[i].calcula();
+		encomendasRelampago[i].print();
+		custR += encomendasRelampago[i].calcula();
+		std::cout << std::endl;
+	}
+	std::cout << "\n>> Encomendas Normais <<"
+			  << "\nQuantidade: " << encomendasNormal.size()
+			  << "\nValor Total: " << custN
+			  << std::endl
+			  << "\n>> Encomendas Relâmpago <<"
+			  << "\nQuantidade: " << encomendasRelampago.size()
+			  << "\nValor Total: " << custR
+			  << std::endl;
 }
