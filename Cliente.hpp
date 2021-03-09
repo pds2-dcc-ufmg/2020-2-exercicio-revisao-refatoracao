6a924a4ec1467ae2852edfb0e7c9a4e42b9419b6
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 19:43:02 -0300

    Update Cliente.hpp
    Declaração do construtor criado.

--- Cliente.hpp
+++ Cliente.hpp
@@ -9,6 +9,8 @@ class Cliente{
 
 		std::string _nome, _endereco, _cidade, _estado, _cep;
 
+		Cliente(std::string, std::string, std::string, std::string, std::string); //Cria uma instancia de Cliente.
+		Cliente();
 		void print(); // imprime na tela os dados de um cliente cadastrado
 
 };
