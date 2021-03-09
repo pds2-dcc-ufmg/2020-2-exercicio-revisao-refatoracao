f1ad1069d58e29c3dd64a6343479b8caf3cd7c30
thaleshqs <59453636+thaleshqs@users.noreply.github.com>
2021-03-09 16:50:46 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -6,29 +6,39 @@ using namespace std;
 
 class Cliente {
 	public:
-		//imprime na tela os dados de um cliente cadastrado
-		void print(); 
+	//Construtores:
+	Cliente(string _nome , string _endereco , string _cidade , string _estado , string _cep) :
+		nome(_nome), 
+		endereco(_endereco), 
+		cidade(_cidade), 
+		estado(_estado), 
+		cep(_cep) {};
+	Cliente() {};
 	
-		//Métodos getters:
-		string getNome();
-		string getEndereco();
-		string getCidade();
-		string getEstado();
-		string getCep();
+	//imprime na tela os dados de um cliente cadastrado
+	void print(); 
 	
-		//Métodos setters:
-		void setNome(string nome);
-		void setEndereco(string endereco);
-		void setCidade(string cidade);
-		void setEstado(string estado);
-		void setCep(string cep);
+	//Métodos getters:
+	string getNome();
+	string getEndereco();
+	string getCidade();
+	string getEstado();
+	string getCep();
+	
+	//Métodos setters:
+	void setNome(string nome);
+	void setEndereco(string endereco);
+	void setCidade(string cidade);
+	void setEstado(string estado);
+	void setCep(string cep);
+	
 	private:
-		//Atributos do cliente:
-		string nome;
-		string endereco;
-		string cidade;
-		string estado;
-		string cep;
+	//Atributos do cliente:
+	string nome;
+	string endereco;
+	string cidade;
+	string estado;
+	string cep;
 };
 
 #endif
