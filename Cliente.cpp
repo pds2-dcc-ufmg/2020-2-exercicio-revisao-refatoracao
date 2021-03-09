af039125fdb5abf419d94d55765574f57974ade6
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 17:56:55 -0300

    Update Cliente.cpp
    Mudança na identação.

--- Cliente.cpp
+++ Cliente.cpp
@@ -5,9 +5,9 @@
 void Cliente::print(){
 
 	std::cout << "  Nome: " << _nome << std::endl
-		 << "  Endereço: " << _endereco << std::endl
-		 << "  Cidade: " << _cidade << std::endl
-		 << "  Estado: " << _estado << std::endl
-		 << "  CEP: " << _cep << std::endl;
+	<< "  Endereço: " << _endereco << std::endl
+	<< "  Cidade: " << _cidade << std::endl
+	<< "  Estado: " << _estado << std::endl
+	<< "  CEP: " << _cep << std::endl;
 
 }
