8994882344d2e2fc4cc5f5917b45b85d4403dfb0
luizaaraujo1 <77155448+luizaaraujo1@users.noreply.github.com>
2021-03-09 18:45:02 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -5,27 +5,20 @@
 #include "Cliente.hpp"
 using namespace std;
 
-class EncomendaNormal: public Encomenda{
+class EncomendaNormal: public Encomenda {
 
 	public:
-		
-	double calcula(){
-
-		double x = PESO * CUSTOkg;
-
-		return x;
+		double calcula() {
+			double x = PESO * CUSTOkg;
+			return x;
+		}
+		void print() {
+			Encomenda::print();
+			std::cout << "[Encomenda Normal]" << endl;
+			std::cout << "  Peso: " << PESO << endl
+				<< "  Custo por kg: " << CUSTOkg << endl
+				<< "  Custo total: " << T << endl;
 	}
-
-	void print(){
-
-		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
-
-	}
-
 };
 
 #endif
