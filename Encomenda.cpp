e190772a6a7d4adae06c22f957b00542c4a91ff5
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 17:58:59 -0300

    Update Encomenda.cpp
    Mudança na identação.

--- Encomenda.cpp
+++ Encomenda.cpp
@@ -4,17 +4,18 @@
 
 void Encomenda::print(){
 
-			std::cout << "[Remetente]" << std::endl;
-			_remetente.print();
-			std::cout << "[Destinatário]" << std::endl;
-			_dest.print();
-  
+	std::cout << "[Remetente]" << std::endl;
+	_remetente.print();
+
+	std::cout << "[Destinatário]" << std::endl;
+	_dest.print();
+
 }
 
 double Encomenda::calcula(){
-	
+
 	double x = _peso * _custoKg;
 
 	return x;
-	
+
 }
