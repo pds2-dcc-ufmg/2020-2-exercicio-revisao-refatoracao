d66aadd6831c4d4c10f663e5ed19509730bb374f
gabrielbuenoo <53455576+gabrielbuenoo@users.noreply.github.com>
2021-03-09 15:43:06 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -5,23 +5,16 @@
 using namespace std;
 
 class Encomenda{
-
+	private:
+		double _peso = 0.0;
+		double _custo_kg = 0.0;
+		double _custo_total = 0.0;
+		Cliente _remetente;
+		Cliente _destinatario;
+	
 	public:
-
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
-			
-		void print(){
-
-			std::cout << "[Remetente]" << endl;
-			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
-		}
-
+		Encomenda(double peso, double custo_kg, double custo_total, Cliente remetente, Cliente destinatario); // construtor para classe encomenda
+		void print(); // imprime na tela os dados de uma encomenda cadastrada
 };
 
 #endif
