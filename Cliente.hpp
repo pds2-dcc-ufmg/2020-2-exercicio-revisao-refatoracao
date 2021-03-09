b012031d94715d80d290c0893ca60a2c9c46fdde
Vinicius Trindade <71476584+Viniciustda@users.noreply.github.com>
2021-03-09 16:11:47 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -5,14 +5,30 @@
 using namespace std;
 
 class Cliente{
-
-	public:
-
+	
+	private:
 		string NOME;
 		string endereco;
 		string CIDADE;
 		string ESTADO;
 		string cep;
+
+	public:
+
+		string getNome();
+		void setNome(string nome);
+	
+		string getEndereco();
+		void setEndereco(string endereco);
+	
+		string getCidade();
+		void setCidade(string cidade);
+		
+		string getEstado();
+		void setEstado(string estado);
+	
+		string getCep();
+		void setCep(string cep);
 				
 		void print(); // imprime na tela os dados de um cliente cadastrado
 		
