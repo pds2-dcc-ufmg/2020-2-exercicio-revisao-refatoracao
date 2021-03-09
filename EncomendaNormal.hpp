b8cfd0896a7eaba7355697eb604ea9739f19dfee
gabrielbuenoo <53455576+gabrielbuenoo@users.noreply.github.com>
2021-03-09 20:46:41 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -7,13 +7,13 @@ using namespace std;
 
 class EncomendaNormal: public Encomenda{
 	public:
-		double calcula(){
+		double calcula() override{
 			this->_custo_total = this->_peso * this->_custo_kg;
 			return this->_custo_total;
 		} // calcula o valor do custo total da entrega e o retorna
 
-		void print(){
-			Encomenda::print();
+		void print_parte_2 override(){
+			Encomenda::print_parte_1();
 			std::cout << "[Encomenda Normal]" << endl;
 			std::cout << "  Peso: " << this->_peso << endl
 				<< "  Custo por kg: " << this->_custo_kg << endl
