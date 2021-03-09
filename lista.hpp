95aa378f8aa5eca4fa800359a6302b5d900ef6b4
Felipe <73199289+felipezan@users.noreply.github.com>
2021-03-09 02:23:06 -0300

    Update lista.hpp

--- lista.hpp
+++ lista.hpp
@@ -8,7 +8,7 @@ template <class T> class Lista {
 	private:
 
 		No<T> *primeiro; // ponteiro que aponta para o primeiro no de tipo T da lista
-	    No<T> *ultimo; // ponteiro que aponta para o ultimo no de tipo T da lista
+	        No<T> *ultimo; // ponteiro que aponta para o ultimo no de tipo T da lista
 		int _listaSize; // variavel inteira que armazena a quantidade de nos inseridos na lista
 
 
