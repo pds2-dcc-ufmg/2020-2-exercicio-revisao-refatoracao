f05b7394e964320fa2fe325dd681253f4d65acfd
ericksunclair <80356375+ericksunclair@users.noreply.github.com>
2021-03-09 19:52:41 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -8,22 +8,9 @@ class EncomendaNormal: public Encomenda{
 
 	public:
 		
-	double calcula() override{
+	virtual double calcula() override;
 
-		double x = peso * custoKg;
-
-		return x;
-	}
-
-	void print() override{
-
-		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << std::endl;
-		std::cout << "  Peso: " << peso << std::endl
-			<< "  Custo por kg: " << custoKg << std::endl
-			<< "  Custo total: " << custoTotal << std::endl;
-
-	}
+	virtual void print() override;
 
 };
 
