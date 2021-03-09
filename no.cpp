76e1d4b5a8eced9ea1d15dc9ececef592e9626f7
Felipe <73199289+felipezan@users.noreply.github.com>
2021-03-09 02:34:13 -0300

    Update no.cpp

--- no.cpp
+++ no.cpp
@@ -8,13 +8,13 @@ template <class T> No<T>::No(T encomenda) { // construtor de no
 // metodos set
 template <class T> void No<T>::setProximo (No *proximo) { // atribui o proximo no da lista a variavel responsavel por amazena-lo
     this->proximo = proximo;
-}
+} // fim do metodo setProximo
 
 // metodos get
 template <class T> No<T> *No<T>::getProximo () const { // retorna o proximo no
     return this->proximo;
-}
+} // fim do metodo getProximo
 
 template <class T> T No<T>::getEncomenda () const { // retorna a encomenda do tipo T corrente
     return this->encomenda;
-}
+} // fim do metodo getEncomenda
