c7c67d933ad60f8475957724500848144d23ea94
arthurpnader <80353201+arthurpnader@users.noreply.github.com>
2021-03-09 20:48:39 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -4,18 +4,29 @@
 #include <string>
 using namespace std;
 
-class Cliente{
+class Cliente {
 
-	public:
+private:
 
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+	string nome;
+	string endereco;
+	string cidade;
+	string estado;
+	string CEP;
+	
+
+public:
+	Cliente(string nom, string end, string cid, string est, string cep);
+	Cliente(){};
+
+	string getNome();
+	string getEndereco();
+	string getCidade();
+	string getEstado();
+	string getCEP();
+
+	void print(); // imprime na tela os dados de um cliente cadastrado
+
 };
 
 #endif
