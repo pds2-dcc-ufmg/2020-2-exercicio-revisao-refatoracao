80861fdd353019e5d33f41cf8c389fde7945dc90
ericksunclair <80356375+ericksunclair@users.noreply.github.com>
2021-03-09 16:48:31 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -11,3 +11,23 @@ void Cliente::print(){
 		 << "  CEP: " << cep << endl;
 
 }
+
+std::string Cliente::getNome() const { return nome; }
+
+std::string Cliente::getEndereco() const { return endereco; }
+
+std::string Cliente::getCidade() const { return cidade; }
+
+std::string Cliente::getEstado() const { return estado; }
+
+std::string Cliente::getCep() const { return cep; }
+
+void Cliente::setNome(std::string _nome) { nome = _nome; }
+
+void Cliente::setEndereco(std::string _endereco) { endereco = _endereco; }
+
+void Cliente::setCidade(std::string _cidade) { cidade = _cidade; }
+
+void Cliente::setEstado(std::string _estado) { estado = _estado; }
+
+void Cliente::setCep(std::string _cep) { cep = _cep; }
