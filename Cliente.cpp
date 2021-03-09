870a7a54812c8237e9ee7b3e57bcc57065b28f92
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 19:42:02 -0300

    Update Cliente.cpp
    Criei construtores para melhorar a legibilidade do main.

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,6 +2,28 @@
 #include <string>
 #include "Cliente.hpp"
 
+Cliente::Cliente(std::string nome, std::string endereco, std::string cidade, std::string estado, std::string cep){
+
+	_nome = nome;
+	_endereco = endereco;
+	_cidade = cidade;
+	_estado = estado;
+	_cep = cep;
+
+}
+
+Cliente::Cliente(){
+
+	std::string S = "aaa";
+
+	_nome = S;
+	_endereco = S;
+	_cidade = S;
+	_estado = S;
+	_cep = S;
+
+}
+
 void Cliente::print(){
 
 	std::cout << "  Nome: " << _nome << std::endl
