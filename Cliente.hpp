9664db01ececbeb22e6d239c7c0f983263d10e3a
ericksunclair <80356375+ericksunclair@users.noreply.github.com>
2021-03-09 16:28:29 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,15 +7,26 @@ using namespace std;
 class Cliente{
 
 	public:
-
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
+		void getNome();	// retorna o nome do cliente
+		void getEndereco();	// retorna o endereco do cliente
+		void getCidade();	// retorna a cidade do cliente
+		void getEstado();	// retorna o estado do cliente
+		void getCep();	// retorna o cep do cliente
+		
+		void setNome();	// define o nome do cliente
+		void setEndereco();	// define o endereco do cliente
+		void setCidade();	// define a cidade do cliente
+		void setEstado();	// define o estado do cliente
+		void setCep();	// define o cep do cliente
+		
 		void print(); // imprime na tela os dados de um cliente cadastrado
 		
+	private:
+		string nome;
+		string endereco;
+		string cidade;
+		string estado;
+		string cep;
 };
 
 #endif
