92d1982696dfd2a3351a05f65a6502421afbc34d
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 18:00:32 -0300

    Update EncomendaNormal.cpp
    Mudança na identação.

--- EncomendaNormal.cpp
+++ EncomendaNormal.cpp
@@ -4,18 +4,18 @@
 
 double EncomendaNormal::calcula(){
 
-		return Encomenda::calcula();
-  
+	return Encomenda::calcula();
+
 }
 
 void EncomendaNormal::print(){
 
-		Encomenda::print();
-  
-		std::cout << "[Encomenda Normal]" << std::endl;
-  
-		std::cout << "  Peso: " << _peso << std::endl
-			<< "  Custo por kg: " << _custoKg << std::endl
-			<< "  Custo total: " << _total << std::endl;
+	Encomenda::print();
+
+	std::cout << "[Encomenda Normal]" << std::endl;
+
+	std::cout << "  Peso: " << _peso << std::endl
+	<< "  Custo por kg: " << _custoKg << std::endl
+	<< "  Custo total: " << _total << std::endl;
 
 }
