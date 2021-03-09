d34efd9afc0b933b9b10e0c4b694da104d9525ac
ericksunclair <80356375+ericksunclair@users.noreply.github.com>
2021-03-09 17:19:23 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -2,26 +2,25 @@
 #define Encomenda_H
 
 #include "Cliente.hpp"
-using namespace std;
 
 class Encomenda{
 
 	public:
-
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
-			
+		
 		void print(){
-
-			std::cout << "[Remetente]" << endl;
+			
+			std::cout << "[Remetente]" << std::endl;
 			remetente.print();
-			std::cout << "[Destinatário]" << endl;
-			dest.print();
+			std::cout << "[Destinatário]" << std::endl;
+			destinatario.print();
 		}
 
+	protected:
+		double peso = 0.0;
+		double custoKg = 0.0;
+		double pesoTotal = 0.0;
+		Cliente remetente;
+		Cliente destinatario;
 };
 
 #endif
