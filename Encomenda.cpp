03091e801c79e6a725b4d809687eee1e270c253a
IMPedrosa <48529266+IMPedrosa@users.noreply.github.com>
2021-03-09 19:43:54 -0300

    Update Encomenda.cpp
    Criei construtor para melhorar legibilidade do main.

--- Encomenda.cpp
+++ Encomenda.cpp
@@ -2,19 +2,28 @@
 #include <string>
 #include "Encomenda.hpp"
 
+Encomenda::Encomenda(double peso, double custoKg, Cliente &remetente, Cliente &dest){
+
+	this->_peso = peso;
+	this->_custoKg = custoKg;
+	this->_remetente = remetente;
+	this->_dest = dest;	
+
+}
+
 void Encomenda::print(){
 
 	std::cout << "[Remetente]" << std::endl;
-	_remetente.print();
+	this->_remetente.print();
 
 	std::cout << "[Destinatário]" << std::endl;
-	_dest.print();
+	this->_dest.print();
 
 }
 
 double Encomenda::calcula(){
 
-	double x = _peso * _custoKg;
+	double x = this->_peso * this->_custoKg;
 
 	return x;
 
