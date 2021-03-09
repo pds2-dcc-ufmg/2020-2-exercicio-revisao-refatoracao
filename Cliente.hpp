8fd0c1a70688241a9f32fab180eb093a25807a3d
Shirlley111 <79946461+Shirlley111@users.noreply.github.com>
2021-03-09 00:38:13 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -8,15 +8,17 @@ using namespace std;
 class Cliente{ //Classe com os dados do cliente. 
 
 	public:
-
+               void print(); // imprime na tela os dados de um cliente cadastrado
+		
+	
+	private:
 		string NOME;
 		string endereco;
 		string CIDADE;
 		string ESTADO;
 		string cep;
 				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+	      
 };
 
 #endif
