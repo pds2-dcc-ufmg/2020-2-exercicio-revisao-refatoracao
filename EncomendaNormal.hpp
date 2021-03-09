606172f4540b5f0ca1b900f6e082b24bc1871046
gabrielbuenoo <53455576+gabrielbuenoo@users.noreply.github.com>
2021-03-09 16:16:21 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -10,7 +10,7 @@ class EncomendaNormal: public Encomenda{
 		double calcula(){
 			this->_custo_total = this->_peso * this->_custo_kg;
 			return this->_custo_total;
-		}
+		} // calcula o valor do custo total da entrega e o retorna
 
 		void print(){
 			Encomenda::print();
@@ -18,7 +18,7 @@ class EncomendaNormal: public Encomenda{
 			std::cout << "  Peso: " << this->_peso << endl
 				<< "  Custo por kg: " << this->_custo_kg << endl
 				<< "  Custo total: " << this->_custo_total << endl;
-		}
+		} // imprime na tela informações sobre a encomenda cadastrada
 };
 
 #endif
