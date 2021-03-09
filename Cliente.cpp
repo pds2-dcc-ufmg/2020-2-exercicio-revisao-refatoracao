2a096ad831e41ea47a83f06c9a15695a32ed2530
kare-nina <karenina.favoreto@hotmail.com>
2021-03-09 15:48:51 -0300

    public to private atributes + get/set on cliente.

--- Cliente.cpp
+++ Cliente.cpp
@@ -11,3 +11,43 @@ void Cliente::print(){
 		 << "  CEP: " << cep << endl;
 
 }
+
+void getNome(){
+    return this->nome;
+}
+
+void setNome(std::string n){
+    this->nome = n;
+}
+
+void getEndereco(){
+    return this->endereco;
+}
+
+void setEndereco(std::string e){
+    this->endereco = e;
+}
+
+void getCidade(){
+    return this->cidade;
+}
+
+void setCidade(std::string c){
+    this->cidade = c;
+}
+
+void getEstado(){
+    return this->estado;
+}
+
+void setEstado(std::string s){
+    this->estado = s;
+}
+
+void getCep(){
+    return this->cep;
+}
+
+void setCep(std::string cp){
+    this->cep = cp;
+}
--- Cliente.hpp
+++ Cliente.hpp
@@ -5,15 +5,27 @@
 
 class Cliente{
 
-	public:
+	private:
 
 		std::string nome;
 		std::string endereco;
 		std::string cidade;
 		std::string estado; 
 		std::string cep;
-				
+    
+    public:
 		void print(); // imprime na tela os dados de um cliente cadastrado
+        void getNome();
+        void setNome(std::string n);
+        void getEndereco();
+        void setEndereco(std::string e);
+        void getCidade();
+        void setCidade(std::string c);
+        void getEstado();
+        void setEstado(std::string s);
+        void getCep();
+        void setCep(std::string cp);
+    
 		
 };
 
