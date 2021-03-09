41d491962aea92a92abd07e377f023ed09852893
gabrielbuenoo <53455576+gabrielbuenoo@users.noreply.github.com>
2021-03-09 15:51:46 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -13,7 +13,7 @@ class Encomenda{
 		Cliente _destinatario;
 	
 	public:
-		Encomenda(double peso, double custo_kg, double custo_total, Cliente remetente, Cliente destinatario); // construtor para classe encomenda
+		Encomenda(double peso, double custo_kg, Cliente remetente, Cliente destinatario); // construtor para classe encomenda
 		void print(); // imprime na tela os dados de uma encomenda cadastrada
 };
 
