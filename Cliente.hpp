f0dcc163530d74d52e2427c96a9ad9fe493e8bb2
Vinicius Trindade <71476584+Viniciustda@users.noreply.github.com>
2021-03-09 20:40:35 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -15,20 +15,20 @@ class Cliente{
 
 	public:
 
-		string getNome();
-		void setNome(string new_nome);
+		string getNome();  //retorna o nome
+		void setNome(string new_nome);  //altera o nome
 	
-		string getEndereco();
-		void setEndereco(string new_endereco);
+		string getEndereco();  //retorna o endereco
+		void setEndereco(string new_endereco);  //altera o endereco
 	
-		string getCidade();
-		void setCidade(string new_cidade);
+		string getCidade();  //retorna a cidade
+		void setCidade(string new_cidade); //altera a cidade
 		
-		string getEstado();
-		void setEstado(string new_estado);
+		string getEstado();  //retorna o estado
+		void setEstado(string new_estado);  //altera o estado
 	
-		string getCep();
-		void setCep(string new_cep);
+		string getCep();  //retorna o cep
+		void setCep(string new_cep);  //altera o cep
 				
 		void print(); // imprime na tela os dados de um cliente cadastrado
 		
