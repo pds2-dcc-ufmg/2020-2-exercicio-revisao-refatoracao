f657a6a2c97577b5add282102e55a64b31bf6e37
ericksunclair <80356375+ericksunclair@users.noreply.github.com>
2021-03-09 17:35:31 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -7,7 +7,7 @@ class Encomenda{
 
 	public:
 		
-		void print(){
+		virtual void print(){
 			
 			std::cout << "[Remetente]" << std::endl;
 			remetente.print();
@@ -15,10 +15,20 @@ class Encomenda{
 			destinatario.print();
 		}
 
+		virtual double calcula(){
+			return 0;	
+		}
+	
+		double getPeso() const { return peso; }
+		double getCustoKg() const { return custoKg; }
+		double getCustoTotal() const { return custoTotal; }
+		Cliente getRemetente() const { return remetente; }
+		Cliente getDestinatario() const { return destinatario; }
+	
 	protected:
 		double peso = 0.0;
 		double custoKg = 0.0;
-		double pesoTotal = 0.0;
+		double custoTotal = 0.0;
 		Cliente remetente;
 		Cliente destinatario;
 };
