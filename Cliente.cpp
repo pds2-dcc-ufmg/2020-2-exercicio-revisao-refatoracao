8856ccfe14921000396f9c1364f73932eac86d83
Felipe <goncalves.pipo2123@gmail.com>
2021-03-09 19:27:48 -0300

    refatorações

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,10 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
-
-	std::cout << "  Nome: " << NOME << endl
+void Cliente::print() {
+	cout << "  Nome: " << nome << endl
 		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
+		 << "  Cidade: " << cidade << endl
+		 << "  Estado: " << estado << endl
 		 << "  CEP: " << cep << endl;
-
 }
