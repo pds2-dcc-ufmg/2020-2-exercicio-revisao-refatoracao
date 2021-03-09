ca8fb552e672e72bff6d16330829108be033a9ab
FernandoAxer <80357271+FernandoAxer@users.noreply.github.com>
2021-03-09 17:47:46 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -4,32 +4,32 @@
 
 using namespace std;
 
-string Cliente::getNome() { return _NOME };
+string Cliente::getNome() { return _NOME; };
 
-string Cliente::getEndereco() { return _ENDERECO };
+string Cliente::getEndereco() { return _ENDERECO; };
 
-string Cliente::getCidade() { return _CIDADE };
+string Cliente::getCidade() { return _CIDADE; };
 
-string Cliente::getEstado() { return _ESTADO };
+string Cliente::getEstado() { return _ESTADO; };
 
-string Cliente::getCep() { return _CEP };
+string Cliente::getCep() { return _CEP; };
 
-void Cliente::setNome(string NOME) { _NOME = NOME };
+void Cliente::setNome(string NOME) { _NOME = NOME; };
 
-void Cliente::setEndereco(string ENDERECO) { _ENDERECO = ENDERECO };
+void Cliente::setEndereco(string ENDERECO) { _ENDERECO = ENDERECO; };
 
-void Cliente::setCidade(string CIDADE) { _CIDADE = CIDADE };
+void Cliente::setCidade(string CIDADE) { _CIDADE = CIDADE; };
 
-void Cliente::setEstado(string ESTADO) { _ESTADO = ESTADO };
+void Cliente::setEstado(string ESTADO) { _ESTADO = ESTADO; };
 
-void Cliente::setCep(stirng CEP) { _CEP = CEP };
+void Cliente::setCep(string CEP) { _CEP = CEP; };
 
 void Cliente::print() {
 
-	cout << "  Nome: " << getNome() << endl
-	cout << "  Endereço: " << getEndereco() << endl
-	cout << "  Cidade: " << getCidade() << endl
-	cout << "  Estado: " << getEstado() << endl
+	cout << "  Nome: " << getNome() << endl;
+	cout << "  Endereço: " << getEndereco() << endl;
+	cout << "  Cidade: " << getCidade() << endl;
+	cout << "  Estado: " << getEstado() << endl;
 	cout << "  CEP: " << getCep() << endl;
 
 }
