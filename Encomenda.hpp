ecc0f8d7ae0af2a95b3857328ced324b95d46746
Rafael Santos <64794722+rafaelvinisan@users.noreply.github.com>
2021-03-09 19:16:13 -0300

    Indentação

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -6,31 +6,27 @@ using namespace std;
 
 class Encomenda{
 
-    protected:
+protected:
 
     double peso = 0.0;
-		double custo_kg = 0.0;
-		double custo_total = 0.0;
-		Cliente remetente;
-		Cliente destinatario;
+    double custo_kg = 0.0;
+    double custo_total = 0.0;
+    Cliente remetente;
+    Cliente destinatario;
 
     Encomenda(double peso, double custo, Cliente destinatario, Cliente remetente) : destinatario(destinatario), remetente(remetente) {
         this->peso = peso;
         this->custo_kg = custo;
     }
 
-    public:
-		void print() {
+public:
+    void print() {
 
-            std::cout << "[Remetente]" << endl;
-            remetente.print();
-            std::cout << "[Destinatário]" << endl;
-            destinatario.print();
-        }
-
-
-
-
+        std::cout << "[Remetente]" << endl;
+        remetente.print();
+        std::cout << "[Destinatário]" << endl;
+        destinatario.print();
+    }
 
 };
 
