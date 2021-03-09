ff169961b0e2e474a5cc2c8b706fc9d147f749bd
matheus-amorimb <74259451+matheus-amorimb@users.noreply.github.com>
2021-03-09 19:52:34 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -13,8 +13,8 @@ class Cliente
 		string estado;
 		string cep;
 		
+		Cliente(){};
 		Cliente (string nome, string endereco, string cidade, string estado, string cep);
-		
 		void print(); // imprime na tela os dados de um cliente cadastrado
 };
 
