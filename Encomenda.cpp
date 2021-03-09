773ff99771a016efe501cd1d19817cc448246ea4
Vinicius Trindade <71476584+Viniciustda@users.noreply.github.com>
2021-03-09 20:11:45 -0300

    Update Encomenda.cpp

--- Encomenda.cpp
+++ Encomenda.cpp
@@ -3,36 +3,36 @@
 #include "Encomenda.hpp"
 
 double Encomenda::getPeso() {
-  return this->peso;
+	return this->peso;
 }
 void Encomenda::setPeso(double new_peso) {
-  this->peso = new_peso;
+	this->peso = new_peso;
 }
 	
 double Encomenda::getCustoKg() {
-  return this->custo_kg;
+	return this->custo_kg;
 }
 void Encomenda::setCustoKg(double new_custo_kg) {
-  this->custo_kg = new_custo_kg;
+	this->custo_kg = new_custo_kg;
 }
 	
 double Encomenda::getCustoTotal(double taxa) {
-  double taxa_adicional = taxa + 1.0;
-  return this->peso * this->custo_kg * taxa_adicional;
+	double taxa_adicional = taxa + 1.0;
+	return this->peso * this->custo_kg * taxa_adicional;
 }
 	
 Cliente Encomenda::getRemetente() {
-  return this->remetente;
+	return this->remetente;
 }
 void Encomenda::setRemetente(Cliente new_remetente) {
-  this->remetente = new_remetente;
+	this->remetente = new_remetente;
 }
 	
 Cliente Encomenda::getDest() {
-  return this->dest;
+	return this->dest;
 }
 void Encomenda::setDest(Cliente new_dest) {
-  this->dest = new_dest;
+	this->dest = new_dest;
 }
 			
 void Encomenda::print() {
