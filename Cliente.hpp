e978ab541bf6b0ee045c5f754b1ed0f6415ae525
diegovieirdos <80367453+diegovieirdos@users.noreply.github.com>
2021-03-10 11:23:25 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,20 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
-class Cliente{
+class Cliente
+{
+    private:
+        std::string nome;
+        std::string endereco;
+        std::string cidade;
+        std::string estado;
+        std::string cep;
 
-	public:
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
-	private:
-		string NOME;
-		string ENDERECO;
-		string CIDADE;
-		string ESTADO;
-		string CEP;
-		
+    public:
+        Cliente(std::string _nome, std::string _endereco, std::string _cidade, std::string _estado, std::string _cep): nome(_nome), endereco(_endereco), cidade(_cidade), estado(_estado), cep(_cep){}
+        Cliente(){}
+        void print(); // imprime na tela os dados de um cliente cadastrado
 };
 
 #endif
