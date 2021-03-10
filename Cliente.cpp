e13ac823bb1a8d15b60f83c3e1ce37bdf01e2af1
jmatoss1 <75380969+jmatoss1@users.noreply.github.com>
2021-03-09 23:16:51 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,75 @@
 #include <string>
 #include "Cliente.hpp"
 
+Cliente::Cliente(string _nome, string _endereco, string _cidade, string _estado, string _cep){
+	
+	this->setNome(_nome);
+	this->setEndereco(_endereco);
+	this->setCidade(_cidade);
+	this->setEstado(_estado);
+	this->setCep(_cep);
+	
+}
+
 void Cliente::print(){
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+	std::cout << "  Nome: " << this->getNome() << endl
+		 << "  Endereço: " << this->getEndereco() << endl
+		 << "  Cidade: " << this->getCidade() << endl
+		 << "  Estado: " << this->getEstado() << endl
+		 << "  CEP: " << this->getCep() << endl;
 
 }
+
+void Cliente::setNome(string _nome){
+
+	this->nome=_nome;	
+}
+
+void Cliente::setEndereco(string _endereco){
+
+	this->endereco=_endereco;
+}
+
+void Cliente::setCidade(string _cidade){
+
+	this->cidade=_cidade;
+}
+
+void Cliente::setEstado(string _estado){
+
+	this->estado=_estado;
+}
+
+void Cliente::setCep(string _cep){
+
+	this->cep=_cep;
+}
+
+string Cliente::getNome(){
+
+	return this->nome;
+}
+
+string Cliente::getEndereco(){
+
+	return this->endereco;
+}
+
+string Cliente::getCidade(){
+
+	return this->cidade;
+}
+
+string Cliente::getEstado(){
+
+	return this->estado;
+}
+
+string Cliente::getCep(){
+
+	return this->cep;
+}
+
+
+
