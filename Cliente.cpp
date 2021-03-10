60364366a3de9a14f403b5f5b7bee33b08abfc4a
Guilherme Drumond Rosa <gdrumondrosa@gmail.com>
2021-03-09 22:27:59 -0300

    Mudanças Client.cpp
    Arrumando identação do código, nome de variáveis e construtor da classe Cliente

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,18 @@
 #include <string>
 #include "Cliente.hpp"
 
+Cliente::Cliente(std::string _nome, std::string _endereco, std::string _cidade, std::string _estado, std::string _cep) {
+	this->nome = _nome;
+	this->endereco = _endereco;
+	this->cidade = _cidade;
+	this->estado = _estado;
+	this->cep = _cep;
+}
+
 void Cliente::print(){
-
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
-
+	std::cout << "  Nome: " << nome << std::endl
+	          << "  Endereço: " << endereco << std::endl
+		  << "  Cidade: " << cidade << std::endl
+		  << "  Estado: " << estado << std::endl
+		  << "  CEP: " << cep << std::endl;
 }
