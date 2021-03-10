8e173cc578ed747ce2c83f70e8015dd5dc48ab89
diegovieirdos <80367453+diegovieirdos@users.noreply.github.com>
2021-03-10 11:28:21 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -1,37 +1,24 @@
 #ifndef EncomendaNormal_H
 #define EncomendaNormal_H
-
+#include <iostream>
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
-using namespace std;
 
 class EncomendaNormal: public Encomenda{
 
-	public:
-	
-	EncomendaNormal(double weight, double cost_per_kg, Cliente sender,  Cliente recipient): 		Encomenda(weight, cost_per_kg, sender, recipient){}
-	
-	int getClassType() override{
-		return 1;
-	}
+public:
+	double calcula(){
+	    return (peso * custoKg);
+    }
 
-	double calcula() override{
-
-		double x = PESO * CUSTOkg;
-
-		return x;
-	}
-
-	void print() override{
-
+	void print(){
 		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
 
+        	std::cout  << "[Encomenda Normal] " << std::endl
+		           << "  Peso: " << peso << std::endl
+		           << "  Custo por kg: " << custoKg << std::endl
+		           << "  Custo total: " << custoTotal << std::endl;
 	}
-
 };
 
 #endif
