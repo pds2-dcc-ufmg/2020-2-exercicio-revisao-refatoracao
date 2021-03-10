a7cfc909ee4e4706df5750451d9f03ecbf803f45
bernardoacp <63015851+bernardoacp@users.noreply.github.com>
2021-03-09 21:10:29 -0300

    Update EncomendaRelampago.hpp

--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -8,26 +8,24 @@ using namespace std;
 class EncomendaRelampago: public Encomenda{
 
 	public:
-
-		double calcula(){
-
-			double x = PESO * CUSTOkg;
-			x += x * 0.25;
-			
+		double calcula() override {
+			double x = this->_peso * this->_custoKg;
+			x += x * this->_taxa;
 			return x;
 		}
 
 		void print(){
-
 			Encomenda::print();
 			std::cout << "[Encomenda Relâmpago]" << endl;
-			std::cout << "  Peso: " << PESO << endl
-				<< "  Custo por kg: " << CUSTOkg << endl
-				<< "  Taxa adicional: " << 0.25 << endl
-				<< "  Custo total: " << T << endl;
+			std::cout << "  Peso: " << this->_peso << endl
+				<< "  Custo por kg: " << this->_custoKg << endl
+				<< "  Taxa adicional: " << this->_taxa << endl
+				<< "  Custo total: " << this->_total << endl;
 
 		}
-
+	
+	private:
+		static constexpr double taxa = 0.25;
 };
 
 #endif
