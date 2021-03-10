4181896b77c58ffe14128ed9740cc557d04301fe
diegovieirdos <80367453+diegovieirdos@users.noreply.github.com>
2021-03-10 11:31:43 -0300

    Update EncomendaRelampago.hpp

--- EncomendaRelampago.hpp
+++ EncomendaRelampago.hpp
@@ -1,41 +1,25 @@
-#ifndef EncomendaRelampago_H
-#define EncomendaRelampago_H
-
+#ifndef EncomendaNormal_H
+#define EncomendaNormal_H
+#include <iostream>
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
-using namespace std;
 
-#define PRICE_INCREASE_RELAMPAGO 0.25
+class EncomendaNormal: public Encomenda
+{
 
-class EncomendaRelampago: public Encomenda{
+    public:
+        double calcula(){
+            return (peso * custoKg);
+        }
 
-	public:
+    void print(){
+        Encomenda::print();
 
-		EncomendaRelampago(double weight, double cost_per_kg, Cliente sender,  Cliente recipient): 	Encomenda(weight, cost_per_kg, sender, recipient){}
-
-		int getClassType() override{
-			return 2;
-		}
-
-		double calcula() override{
-
-			double x = PESO * CUSTOkg;
-			x += x * PRICE_INCREASE_RELAMPAGO;
-			
-			return x;
-		}
-
-		void print() override{
-
-			Encomenda::print();
-			std::cout << "[Encomenda Relâmpago]" << endl;
-			std::cout << "  Peso: " << PESO << endl
-				<< "  Custo por kg: " << CUSTOkg << endl
-				<< "  Taxa adicional: " << 0.25 << endl
-				<< "  Custo total: " << T << endl;
-
-		}
-
+        std::cout  << "[Encomenda Normal] " << std::endl
+		           << "  Peso: " << peso << std::endl
+		           << "  Custo por kg: " << custoKg << std::endl
+		           << "  Custo total: " << custoTotal << std::endl;
+    }
 };
 
 #endif
