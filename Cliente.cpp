1475952fb6551ace2e9b4f6918ae79a58579978c
Marcos-Ribas <80366681+Marcos-Ribas@users.noreply.github.com>
2021-03-09 21:31:02 -0300

    Update Cliente.cpp

--- Cliente.cpp
+++ Cliente.cpp
@@ -4,10 +4,30 @@
 
 void Cliente::print(){
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+	std::cout << "  Nome: " << this->getNome() << endl
+		 << "  Endereço: " << this->getEndereco() << endl
+		 << "  Cidade: " << this->getCidade() << endl
+		 << "  Estado: " << this->getEstado() << endl
+		 << "  CEP: " << this->getCep() << endl;
 
+}
+void Cliente::getNome(){
+	return nome;
+	
+}
+void Cliente::getEndereco(){
+	return endereco;
+		
+}
+void Cliente::getCidade(){
+ 	return cidade;
+		
+}
+void Cliente::getEstado(){
+	return estado;
+		
+}
+void Cliente::getCep(){
+	return cep;
+		
 }
