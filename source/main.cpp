1874a93a0c086d7731512b72840ed03706d1e575
Raphael Leivas <raphaelleivas@cpejr.com.br>
2021-03-09 22:17:49 -0300

    FINALIZADO

--- source/main.cpp
+++ source/main.cpp
@@ -9,68 +9,19 @@
 using namespace std;
 
 int main(){
-	
-	Cliente* cli0 = new Cliente(
-    "Thales",
-    "Rua dos Pré-Socráticos",
-    "Miletus",
-    "Ionia",
-    "548 a.C.");
+  
+	// declara os clientes todos
+	Cliente* cli0 = new Cliente ( "Thales", "Rua dos Pré-Socráticos", "Miletus", "Ionia", "548 a.C." );
+  Cliente* cli1 = new Cliente ( "Aristóteles", "Avenida do Meio-Termo", "Atenas", "Ática", "384 a.C." );
+  Cliente* cli2 = new Cliente ( "Platão", "Praça das Formas", "Atenas", "Ática", "348 a.C" );
+  Cliente* cli3 = new Cliente ( "Sócrates", "Rua do Elenchus", "Atenas", "Ática", "399 a.C." );
+  Cliente* cli4 = new Cliente ( "Pitágoras", "Praça dos Quadrados dos Catetos", "Samos", "Egeu", "571 a.C." );
+  Cliente* cli5 = new Cliente ( "Parmênides", "Rua do Não Ser", "Eleia", "Magna Grécia", "460 a.C." );
+  Cliente* cli6 = new Cliente ( "Empédocles", "Rua dos Quatro Elementos", "Agrigento", "Sicília", "495 a.C." );
+  Cliente* cli7 = new Cliente ( "Anaxágoras", "Avenida da Mente Cósmica", "Clazômenas", "Jónia", "499 a.C." );
 
-  Cliente* cli1 = new Cliente(
-    "Aristóteles",
-    "Avenida do Meio-Termo",
-    "Atenas",
-    "Ática",
-    "384 a.C.");
-
-  Cliente* cli2 = new Cliente (
-    "Platão",
-    "Praça das Formas",
-    "Atenas",
-    "Ática",
-    "348 a.C");
-
-  Cliente* cli3 = new Cliente (
-    "Sócrates",
-    "Rua do Elenchus",
-    "Atenas",
-    "Ática",
-    "399 a.C.");
-
-  Cliente* cli4 = new Cliente (
-    "Pitágoras",
-    "Praça dos Quadrados dos Catetos",
-    "Samos",
-    "Egeu",
-    "571 a.C.");
-
-  Cliente* cli5 = new Cliente (
-    "Parmênides",
-    "Rua do Não Ser",
-    "Eleia",
-    "Magna Grécia",
-    "460 a.C.");
-
-  Cliente* cli6 = new Cliente (
-    "Empédocles",
-    "Rua dos Quatro Elementos",
-    "Agrigento",
-    "Sicília",
-    "495 a.C.");
-
-  Cliente* cli7 = new Cliente (
-    "Anaxágoras",
-    "Avenida da Mente Cósmica",
-    "Clazômenas",
-    "Jónia",
-    "499 a.C.");
-
-	unsigned int quantN = 0, quantR = 0;
-  double costN = 0, costR = 0;
-
+  // declara as encomendas todas
   vector<Encomenda*> vec(8);
-
   vec[0] = new EncomendaNormal(5, 12, *cli0, *cli1);
   vec[1] = new EncomendaNormal(10, 12, *cli1, *cli2);
   vec[2] = new EncomendaNormal(7, 12, *cli2, *cli3);
@@ -81,6 +32,9 @@ int main(){
   vec[7] = new EncomendaRelampago(8, 18, *cli7, *cli0);
 	
 	std::cout << "\n>> Relatório de encomendas <<" << endl;
+
+  unsigned int quantN = 0, quantR = 0;
+  double costN = 0, costR = 0;
 
   for(unsigned int i = 0; i < 8; ++i) {
     vec[i]->print();
@@ -105,4 +59,5 @@ int main(){
 		<< "\nQuantidade: " << quantR
 		<< "\nValor Total: " << costR
 		<< endl;
+
 }
