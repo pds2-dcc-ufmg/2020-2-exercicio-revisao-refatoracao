0c11b4b20e9e7370b36bbeeb091aff59fe9dbd97
Guilherme Drumond Rosa <gdrumondrosa@gmail.com>
2021-03-09 22:36:30 -0300

    Mudanças EncomendaNormal.hpp
    Retirada do "using namespace std" e identação.

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -3,27 +3,21 @@
 
 #include "Encomenda.hpp"
 #include "Cliente.hpp"
-using namespace std;
 
 class EncomendaNormal: public Encomenda{
 
 	public:
-		
 	double calcula(){
-
 		double x = PESO * CUSTOkg;
-
 		return x;
 	}
 
 	void print(){
-
 		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
-
+		std::cout << "[Encomenda Normal]" << std::endl
+		          << "  Peso: " << PESO << std::endl
+			  << "  Custo por kg: " << CUSTOkg << std::endl
+		          << "  Custo total: " << T << std::endl;
 	}
 
 };
