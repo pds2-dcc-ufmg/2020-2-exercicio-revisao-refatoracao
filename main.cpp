77ed87e3e9e0ff3b75ab72119fa0f369f28c972c
arthurhsf888 <60933229+arthurhsf888@users.noreply.github.com>
2021-03-09 21:30:41 -0300

    2020054200_ArthurHenriqueSecundinoFerreira
    Excluí o arquivo Cliente.cpp, que continha apenas a implementação do método "print",
    e coloquei a implementação no próprio Cliente.hpp
    
    Excluí a variável "T" declarada no Encomenda.hpp que representava o custo total, pois seu valor
    é identico ao que será retornado no método "calcula" implementado em EncomendaNormal.hpp e
    EncomendaRelampago.hpp, e alterei o nome do método "calcula" para "CustoTotal".
    
    Além de alterar o nome do método "calcula" para "CustoTotal", eu o declarei apenas na superclasse
    Encomenda.hpp, pois ele estava se repetindo em EncomendaNormal e EncomendaRelampago. E como seu valor
    tem uma taxa de 25% na Encomenda Relampago, eu multipliquei o valor
    do método por 1,25 no cout de EncomendaRelampago.hpp.
    
    Retirei os "std" que estavam juntos aos "cout" de todos os arquivos, pois como está declarado
    o namespace std em todos, o std em "std::cout" é desnecessário.
    
    Busquei alinhar o código em determinados pontos, como nas linhas seguidas que continham cout,
    e padronizar os espaços para que o código ficasse menos confuso e mais agradável esteticamente.
    
    No arquivo main.cpp eu além de alterar o nome do método "calcula" que passou a ser "CustoTotal", eu
    multipliquei a variavel custR por 1.25, para se obter a soma correta de todas as encomendas
    relampago.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main(){
-	
+
 	Cliente cli0;
 	cli0.NOME = "Thales";
 	cli0.endereco = "Rua dos Pré-Socráticos";
@@ -70,110 +70,110 @@ int main(){
 	enc0.PESO = 5;
 	enc0.CUSTOkg = 12;
 	enc0.remetente = cli0;
-	enc0.dest = cli1;
+	enc0.destinatario = cli1;
 	quantN++;
 
 	EncomendaNormal enc1;
 	enc1.PESO = 10;
 	enc1.CUSTOkg = 12;
 	enc1.remetente = cli1;
-	enc1.dest = cli2;
+	enc1.destinatario = cli2;
 	quantN++;
 
 	EncomendaNormal enc2;
 	enc2.PESO = 7;
 	enc2.CUSTOkg = 12;
 	enc2.remetente = cli2;
-	enc2.dest = cli3;
+	enc2.destinatario = cli3;
 	quantN++;
 
 	EncomendaNormal enc3;
 	enc3.PESO = 2;
 	enc3.CUSTOkg = 12;
 	enc3.remetente = cli3;
-	enc3.dest = cli4;
+	enc3.destinatario = cli4;
 	quantN++;
 
 	EncomendaNormal enc4;
 	enc4.PESO = 3;
 	enc4.CUSTOkg = 12;
 	enc4.remetente = cli4;
-	enc4.dest = cli5;
+	enc4.destinatario = cli5;
 	quantN++;
 
 	EncomendaRelampago enc5;
 	enc5.PESO = 13;
 	enc5.CUSTOkg = 18;
 	enc5.remetente = cli5;
-	enc5.dest = cli6;
+	enc5.destinatario = cli6;
 	quantR++;
 
 	EncomendaRelampago enc6;
 	enc6.PESO = 6;
 	enc6.CUSTOkg = 18;
 	enc6.remetente = cli6;
-	enc6.dest = cli7;
+	enc6.destinatario = cli7;
 	quantR++;
 
 	EncomendaRelampago enc7;
 	enc7.PESO = 8;
 	enc7.CUSTOkg = 18;
 	enc7.remetente = cli7;
-	enc7.dest = cli0;
+	enc7.destinatario = cli0;
 	quantR++;
-	
-	std::cout << "\n>> Relatório de encomendas <<" << endl;
+
+	cout << "\n>> Relatório de encomendas <<" << endl;
 
 	double custN = 0;
 	double custR = 0;
 
-	enc0.calcula();
+	enc0.CustoTotal();
 	enc0.print();
-	custN += enc0.calcula();
-	std::cout << endl;
+	custN += enc0.CustoTotal();
+	cout << endl;
 
-	enc1.calcula();
+	enc1.CustoTotal();
 	enc1.print();
-	custN += enc1.calcula();
-	std::cout << endl;
+	custN += enc1.CustoTotal();
+	cout << endl;
 
-	enc2.calcula();
+	enc2.CustoTotal();
 	enc2.print();
-	custN += enc2.calcula();
-	std::cout << endl;
+	custN += enc2.CustoTotal();
+	cout << endl;
 
-	enc3.calcula();
+	enc3.CustoTotal();
 	enc3.print();
-	custN += enc3.calcula();
-	std::cout << endl;
+	custN += enc3.CustoTotal();
+	cout << endl;
 
-	enc4.calcula();
+	enc4.CustoTotal();
 	enc4.print();
-	custN += enc4.calcula();
-	std::cout << endl;
+	custN += enc4.CustoTotal();
+	cout << endl;
 
-	enc5.calcula();
+	enc5.CustoTotal();
 	enc5.print();
-	custR += enc5.calcula();
-	std::cout << endl;
+	custR += enc5.CustoTotal();
+	cout << endl;
 
-	enc6.calcula();
+	enc6.CustoTotal();
 	enc6.print();
-	custR += enc6.calcula();
-	std::cout << endl;
+	custR += enc6.CustoTotal();
+	cout << endl;
 
-	enc7.calcula();
+	enc7.CustoTotal();
 	enc7.print();
-	custR += enc7.calcula();
-	std::cout << endl;
-	
-	std::cout << "\n>> Encomendas Normais <<" 
+	custR += enc7.CustoTotal();
+	cout << endl;
+
+	cout << "\n>> Encomendas Normais <<"
 		 << "\nQuantidade: " << quantN
 		 << "\nValor Total: " << custN
 		 << endl
-		 << "\n>> Encomendas Relâmpago <<" 
+		 << "\n>> Encomendas Relâmpago <<"
 		 << "\nQuantidade: " << quantR
-		 << "\nValor Total: " << custR
+		 << "\nValor Total: " << custR * 1.25
 		 << endl;
-	
+
 }
