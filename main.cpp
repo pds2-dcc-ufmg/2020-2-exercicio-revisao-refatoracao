d138950d7134c31d4500c2b089d219dd191696a0
Dionaseto <72622594+Dionaseto@users.noreply.github.com>
2021-03-09 21:50:09 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -122,7 +122,7 @@ int main(){
 	enc7.dest = cli0;
 	quantR++;
 	
-	std::cout << "\n>> Relatório de encomendas <<" << endl;
+	cout << "\n>> Relatório de encomendas <<" << endl;
 
 	double custN = 0;
 	double custR = 0;
@@ -130,22 +130,22 @@ int main(){
 	enc0.calcula();
 	enc0.print();
 	custN += enc0.calcula();
-	std::cout << endl;
+	cout << endl;
 
 	enc1.calcula();
 	enc1.print();
 	custN += enc1.calcula();
-	std::cout << endl;
+	cout << endl;
 
 	enc2.calcula();
 	enc2.print();
 	custN += enc2.calcula();
-	std::cout << endl;
+	cout << endl;
 
 	enc3.calcula();
 	enc3.print();
 	custN += enc3.calcula();
-	std::cout << endl;
+	cout << endl;
 
 	enc4.calcula();
 	enc4.print();
@@ -155,25 +155,22 @@ int main(){
 	enc5.calcula();
 	enc5.print();
 	custR += enc5.calcula();
-	std::cout << endl;
+	cout << endl;
 
 	enc6.calcula();
 	enc6.print();
 	custR += enc6.calcula();
-	std::cout << endl;
+	cout << endl;
 
-	enc7.calcula();
+	enc7.calcula();d
 	enc7.print();
 	custR += enc7.calcula();
-	std::cout << endl;
+	cout << endl;
 	
-	std::cout << "\n>> Encomendas Normais <<" 
-		 << "\nQuantidade: " << quantN
-		 << "\nValor Total: " << custN
-		 << endl
-		 << "\n>> Encomendas Relâmpago <<" 
-		 << "\nQuantidade: " << quantR
-		 << "\nValor Total: " << custR
-		 << endl;
-	
+	cout << "\n>> Encomendas Normais <<"
+	cout << "\nQuantidade: " << quantN
+	cout << "\nValor Total: " << custN << endl
+	cout << "\n>> Encomendas Relâmpago <<" 
+	cout << "\nQuantidade: " << quantR
+	cout << "\nValor Total: " << custR << endl;
 }
