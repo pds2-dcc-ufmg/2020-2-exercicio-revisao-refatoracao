93eac0ac11f6c305ccfa71e3be16ed9f276a7d7a
BrunoLeonardoA <brunoleonardoalmeida@hotmail.com>
2021-03-09 23:45:21 -0300

    Fatoração

--- Cliente.hpp
+++ Cliente.hpp
@@ -6,14 +6,19 @@ using namespace std;
 
 class Cliente{
 
-	public:
-
-		string NOME;
+	private:
+		string nome;
 		string endereco;
-		string CIDADE;
-		string ESTADO;
+		string cidade;
+		string estado;
 		string cep;
 				
+	public:
+		Cliente(string _nome, string _endereco, string _cidade, string _estado, string _cep ) : nome(_nome), endereco(_endereco),
+		cidade(_cidade), estado(_estado), cep(_cep){};
+
+		Cliente::Cliente(){};
+		
 		void print(); // imprime na tela os dados de um cliente cadastrado
 		
 };
