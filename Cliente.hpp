e9a0157a06e2607ad71d047c86dc2166598c78d4
Guilherme Drumond Rosa <gdrumondrosa@gmail.com>
2021-03-09 22:31:31 -0300

    Mudanças Client.hpp
    Remoção do "using namespace std", criação de construtor da classe Cliente, mudança no nome das variáveis e mudança nas permissões dos atributos.

--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,18 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
 class Cliente{
+	private:
+		std::string nome;
+		std::string endereco;
+		std::string cidade;
+		std::string estado;
+		std::string cep;
 
 	public:
-
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+		Cliente(std::string _nome, std::string _endereco, std::string _cidade, std::string _estado, std::string _cep);		
+		void print(); // imprime na tela os dados de um cliente cadastrado		
 };
 
 #endif
