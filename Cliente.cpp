f5fbe0c70c08af8ce4d6dbbc3078557a48da7e58
klayor <72930341+klayor@users.noreply.github.com>
2021-03-09 23:57:28 -0300

    Add files via upload
    Mudanças:
    
    Cliente:
    Variáveis de Publicas para privadas. Novos métodos Get para todas as variáveis.
    
    Mudança no  nome das variáveis para padronização, construtor inicializa os valores numéricos, Novos métodos get e set. Nova variável: Taxa adicional
    
    main.cpp
    Incluídas duas nova biblioteca: String, todas as repetições resumidas em loops, nomes de variáveis trocados e padronizados
    
    
    No geral tentei criar o máximo de métodos possível e padronizar as variáveis para deixar o código mais legível

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,10 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
-
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
-
+void Cliente::printCliente(){
+	std::cout << "  Nome: " << getNome() << endl
+		 << "  Endereço: " << getEndereco() << endl
+		 << "  Cidade: " << getCidade() << endl
+		 << "  Estado: " << getEstado() << endl
+		 << "  CEP: " << getCep() << endl;
 }
