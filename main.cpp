68d3cb20c38355b4b383bfb30166801f4db71de1
Guilherme Drumond Rosa <gdrumondrosa@gmail.com>
2021-03-09 22:41:17 -0300

    Mudanças main.cpp
    Retirada do "using namespace std" e mudança na maneira como os clientes foram criados.

--- main.cpp
+++ main.cpp
@@ -3,65 +3,17 @@
 #include "EncomendaNormal.hpp"
 #include "EncomendaRelampago.hpp"
 #include "Cliente.hpp"
-using namespace std;
 
 int main(){
 	
-	Cliente cli0;
-	cli0.NOME = "Thales";
-	cli0.endereco = "Rua dos Pré-Socráticos";
-	cli0.CIDADE = "Miletus";
-	cli0.ESTADO = "Ionia";
-	cli0.cep = "548 a.C.";
-
-	Cliente cli1;
-	cli1.NOME = "Aristóteles";
-	cli1.endereco = "Avenida do Meio-Termo";
-	cli1.CIDADE = "Atenas";
-	cli1.ESTADO = "Ática";
-	cli1.cep = "384 a.C.";
-
-	Cliente cli2;
-	cli2.NOME = "Platão";
-	cli2.endereco = "Praça das Formas";
-	cli2.CIDADE = "Atenas";
-	cli2.ESTADO = "Ática";
-	cli2.cep = "348 a.C";
-
-	Cliente cli3;
-	cli3.NOME = "Sócrates";
-	cli3.endereco = "Rua do Elenchus";
-	cli3.CIDADE = "Atenas";
-	cli3.ESTADO = "Ática";
-	cli3.cep = "399 a.C.";
-
-	Cliente cli4;
-	cli4.NOME = "Pitágoras";
-	cli4.endereco = "Praça dos Quadrados dos Catetos";
-	cli4.CIDADE = "Samos";
-	cli4.ESTADO = "Egeu";
-	cli4.cep = "571 a.C.";
-
-	Cliente cli5;
-	cli5.NOME = "Parmênides";
-	cli5.endereco = "Rua do Não Ser";
-	cli5.CIDADE = "Eleia";
-	cli5.ESTADO = "Magna Grécia";
-	cli5.cep = "460 a.C.";
-
-	Cliente cli6;
-	cli6.NOME = "Empédocles";
-	cli6.endereco = "Rua dos Quatro Elementos";
-	cli6.CIDADE = "Agrigento";
-	cli6.ESTADO = "Sicília";
-	cli6.cep = "495 a.C.";
-
-	Cliente cli7;
-	cli7.NOME = "Anaxágoras";
-	cli7.endereco = "Avenida da Mente Cósmica";
-	cli7.CIDADE = "Clazômenas";
-	cli7.ESTADO = "Jónia";
-	cli7.cep = "499 a.C.";
+	Cliente cli0("Thales", "Rua dos Pré-Socráticos", "Miletus", "Ionia", "548 a.C.");
+	Cliente cli1("Aristóteles", "Avenida do Meio-Termo", "Atenas", "Ática", "384 a.C.");
+	Cliente cli2("Platão", "Praça das Formas", "Atenas", "Ática", "348 a.C");
+	Cliente cli3("Sócrates", "Rua do Elenchus", "Atenas", "Ática", "399 a.C.");
+	Cliente cli4("Pitágoras", "Praça dos Quadrados dos Catetos", "Samos", "Egeu", "571 a.C.");
+	Cliente cli5("Parmênides", "Rua do Não Ser", "Eleia", "Magna Grécia", "460 a.C.");
+	Cliente cli6("Empédocles", "Rua dos Quatro Elementos", "Agrigento", "Sicília", "495 a.C.");
+	Cliente cli7("Anaxágoras", "Avenida da Mente Cósmica", "Clazômenas", "Jónia", "499 a.C.");
 
 	int quantN = 0;
 	int quantR = 0;
@@ -122,7 +74,7 @@ int main(){
 	enc7.dest = cli0;
 	quantR++;
 	
-	std::cout << "\n>> Relatório de encomendas <<" << endl;
+	std::cout << "\n>> Relatório de encomendas <<" << std::endl;
 
 	double custN = 0;
 	double custR = 0;
@@ -130,50 +82,50 @@ int main(){
 	enc0.calcula();
 	enc0.print();
 	custN += enc0.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc1.calcula();
 	enc1.print();
 	custN += enc1.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc2.calcula();
 	enc2.print();
 	custN += enc2.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc3.calcula();
 	enc3.print();
 	custN += enc3.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc4.calcula();
 	enc4.print();
 	custN += enc4.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc5.calcula();
 	enc5.print();
 	custR += enc5.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc6.calcula();
 	enc6.print();
 	custR += enc6.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 
 	enc7.calcula();
 	enc7.print();
 	custR += enc7.calcula();
-	std::cout << endl;
+	std::cout << std::endl;
 	
 	std::cout << "\n>> Encomendas Normais <<" 
-		 << "\nQuantidade: " << quantN
-		 << "\nValor Total: " << custN
-		 << endl
-		 << "\n>> Encomendas Relâmpago <<" 
-		 << "\nQuantidade: " << quantR
-		 << "\nValor Total: " << custR
-		 << endl;
+		      << "\nQuantidade: " << quantN
+		      << "\nValor Total: " << custN
+		      << std::endl
+		      << "\n>> Encomendas Relâmpago <<" 
+		      << "\nQuantidade: " << quantR
+		  	  << "\nValor Total: " << custR
+		      << std::endl;
 	
 }
