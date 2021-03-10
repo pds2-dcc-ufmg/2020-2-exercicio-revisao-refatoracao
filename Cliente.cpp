918b769d18e8d29f92cda2ccb8b254e62785469d
Gabriel Bordoni <bordoni.gabriel@hotmail.com>
2021-03-09 22:11:50 -0300

    nova alteracao

--- Cliente.cpp
+++ Cliente.cpp
@@ -2,12 +2,23 @@
 #include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+
+Cliente::Cliente(std::string nome, std::string endereco, std::string cidade,std::string estado, std::string cep){	
+	_nome = nome;
+	_endereco = endereco;
+	_cidade = cidade;
+	_estado = estado;
+	_cep = cep;
+}
+
+
+void Cliente::imprimir_dados_pessoais(){
+
+	std::cout << "  Nome: " << _nome << std::endl
+		 << "  Endereço: " << _endereco << std::endl
+		 << "  Cidade: " << _cidade << std::endl
+		 << "  Estado: " << _estado << std::endl
+		 << "  CEP: " << _cep << std::endl;
 
 }
