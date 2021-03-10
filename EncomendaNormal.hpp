a8fa948b4e8645a16f4235eecdf2f5e4430f069e
Marcos-Ribas <80366681+Marcos-Ribas@users.noreply.github.com>
2021-03-09 22:20:00 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -11,7 +11,7 @@ class EncomendaNormal: public Encomenda{
 		
 	double calcula(){
 
-		double x = PESO * CUSTOkg;
+		double x = peso * custoKg;
 
 		return x;
 	}
@@ -20,8 +20,8 @@ class EncomendaNormal: public Encomenda{
 
 		Encomenda::print();
 		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
+		std::cout << "  Peso: " << peso << endl
+			<< "  Custo por kg: " << custoKg << endl
 			<< "  Custo total: " << T << endl;
 
 	}
