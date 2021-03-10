4658471e5c1b77e4ea4a28a3a915144eec433d4d
matheus-amorimb <74259451+matheus-amorimb@users.noreply.github.com>
2021-03-09 22:10:00 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -3,23 +3,31 @@
 
 #include "Cliente.hpp"
 using namespace std;
+using std::string;
 
-class Encomenda{
-
-	public:
-
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
+class Encomenda
+{       
+    	public:
+		double peso = 0.0;
+		double custo_kg = 0.0;
+		double custo_total = 0.0;
 		Cliente remetente;
-		Cliente dest;
+		Cliente destinatario;
+	    
+	        Encomenda(double _peso, double _custo_kg, Cliente _remetente, Cliente _destinatario)
+	        {
+	            	this->peso = _peso;
+	            	this->custo_kg = _custo_kg;
+	            	this->remetente = _remetente;
+			this->destinatario = _destinatario;
+           	 }
 			
-		void print(){
-
+		 void print()
+		{
 			std::cout << "[Remetente]" << endl;
-			remetente.print();
+			this->remetente.print();
 			std::cout << "[Destinatário]" << endl;
-			dest.print();
+			this->destinatario.print();
 		}
 
 };
