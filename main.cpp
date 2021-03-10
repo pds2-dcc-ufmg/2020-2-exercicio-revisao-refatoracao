8e76288b80095a69e5ce31fb55dfc8252246d791
luizaaraujo1 <77155448+luizaaraujo1@users.noreply.github.com>
2021-03-09 22:05:08 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,42 +48,34 @@ int main(){
 	double custN = 0;
 	double custR = 0;
 
-	enc0.calcula();
 	enc0.print();
 	custN += enc0.calcula();
 	std::cout << endl;
 
-	enc1.calcula();
 	enc1.print();
 	custN += enc1.calcula();
 	std::cout << endl;
 
-	enc2.calcula();
 	enc2.print();
 	custN += enc2.calcula();
 	std::cout << endl;
 
-	enc3.calcula();
 	enc3.print();
 	custN += enc3.calcula();
 	std::cout << endl;
-
-	enc4.calcula();
+	
 	enc4.print();
 	custN += enc4.calcula();
 	std::cout << endl;
 
-	enc5.calcula();
 	enc5.print();
 	custR += enc5.calcula();
 	std::cout << endl;
 
-	enc6.calcula();
 	enc6.print();
 	custR += enc6.calcula();
 	std::cout << endl;
 
-	enc7.calcula();
 	enc7.print();
 	custR += enc7.calcula();
 	std::cout << endl;
