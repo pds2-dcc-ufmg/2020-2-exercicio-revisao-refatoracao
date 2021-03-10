ab446626716f2ce555af838c1aa7e6358c5c77c4
matheus-amorimb <74259451+matheus-amorimb@users.noreply.github.com>
2021-03-09 22:06:04 -0300

    Update EncomendaNormal.hpp

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -5,26 +5,32 @@
 #include "Cliente.hpp"
 using namespace std;
 
-class EncomendaNormal: public Encomenda{
-
+class EncomendaNormal: public Encomenda
+{
 	public:
-		
-	double calcula(){
+	    
+		EncomendaNormal(double _peso, double _custo_kg, Cliente _remetente, Cliente _destinatario):Encomenda(_peso, _custo_kg, _remetente, _destinatario)
+	    	{
+	        	this->peso = _peso;
+	        	this->custo_kg = _custo_kg;
+	        	this->remetente = _remetente;
+	        	this->destinatario = _destinatario;
+	    	}
+	    
+	    	double calcula()
+	    	{
+		    	double custo_total = peso * custo_kg;
+		    	return custo_total;
+	    	}
 
-		double x = PESO * CUSTOkg;
-
-		return x;
-	}
-
-	void print(){
-
-		Encomenda::print();
-		std::cout << "[Encomenda Normal]" << endl;
-		std::cout << "  Peso: " << PESO << endl
-			<< "  Custo por kg: " << CUSTOkg << endl
-			<< "  Custo total: " << T << endl;
-
-	}
+	    	void print()
+	    	{
+		    	Encomenda::print();
+		    	cout << "[Encomenda Normal]" << endl;
+		    	cout << "  Peso: " << this->peso << endl
+			     << "  Custo por kg: " << this->custo_kg << endl
+			     << "  Custo total: " << this->custo_total << endl;
+	    	}
 
 };
 
