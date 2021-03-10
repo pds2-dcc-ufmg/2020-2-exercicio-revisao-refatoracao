1c11084daebd9539b7e2f315948c219fb4db2ba3
diegovieirdos <80367453+diegovieirdos@users.noreply.github.com>
2021-03-09 23:08:52 -0300

    Update Cliente.hpp

--- Cliente.hpp
+++ Cliente.hpp
@@ -7,7 +7,7 @@ using namespace std;
 class Cliente{
 
 	public:
-		Cliente(string nome, string endereço, string cidade, string estado, string cep): NOME(nome), ENDERECO(endereco), CIDADE(cidade), ESTADO(estado), CEP(cep){}
+		Cliente(string nome, string endereco, string cidade, string estado, string cep): NOME(nome), ENDERECO(endereco), CIDADE(cidade), ESTADO(estado), CEP(cep){}
 		Cliente(){}
 		void print();
 		
