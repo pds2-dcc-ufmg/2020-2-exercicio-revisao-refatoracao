88ae8f0e68281691af828c1495e6b037bcbdfc5d
nathalialoliveira <79007746+nathalialoliveira@users.noreply.github.com>
2021-03-09 23:06:45 -0300

    espaços e std
    remoção de std's, pois está sendo utilizado "using namespace std" e ajustando espaços

--- EncomendaNormal.hpp
+++ EncomendaNormal.hpp
@@ -10,20 +10,16 @@ class EncomendaNormal: public Encomenda{
 	public:
 		
 	double calcula(){
-
 		double x = PESO * CUSTOkg;
-
 		return x;
 	}
 
 	void print(){
-
 		Encomenda::print();
-		std::cout<< "[Encomenda Normal]"<< endl;
-		std::cout<< "  Peso: "<< PESO<< endl
-			 << "  Custo por kg: "<< CUSTOkg << endl
-			 << "  Custo total: "<< T<< endl;
-
+		cout << "[Encomenda Normal]"<< endl;
+		cout << "  Peso: "<< PESO<< endl
+		     << "  Custo por kg: "<< CUSTOkg << endl
+		     << "  Custo total: "<< T<< endl;
 	}
 
 };
