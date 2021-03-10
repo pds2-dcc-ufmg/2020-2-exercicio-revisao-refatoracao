e31e0ae2b47078df87230fb0e6430f4638102acf
ruybragafilho <ruybragafilho@gmail.com>
2021-03-09 22:27:03 -0300

    Alteracao Classe Cliente

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include <string>
 #include "Cliente.hpp"
 
-void Cliente::print(){
 
-	std::cout << "  Nome: " << NOME << endl
-		 << "  Endereço: " << endereco << endl
-		 << "  Cidade: " << CIDADE << endl
-		 << "  Estado: " << ESTADO << endl
-		 << "  CEP: " << cep << endl;
+// Definição do metodo print da classe Cliente
+void Cliente::print() {
+
+	std::cout << "  Nome: "     << this->_nome     << std::endl
+		      << "  Endereço: " << this->_endereco << std::endl
+		      << "  Cidade: "   << this->_cidade   << std::endl
+		      << "  Estado: "   << this->_estado   << std::endl
+		      << "  CEP: "      << this->_cep      << std::endl;
 
 }
--- Cliente.hpp
+++ Cliente.hpp
@@ -2,20 +2,36 @@
 #define CLIENTE_H
 
 #include <string>
-using namespace std;
 
-class Cliente{
+/*
+ * Classe Cliente - Representa o cadastro de um cliente 
+ */
+class Cliente {
 
 	public:
 
-		string NOME;
-		string endereco;
-		string CIDADE;
-		string ESTADO;
-		string cep;
-				
-		void print(); // imprime na tela os dados de um cliente cadastrado
-		
+        // construtor da classe Cliente
+        Cliente( std::string nome,
+		         std::string endereco,
+		         std::string cidade,
+		         std::string estado,
+		         std::string cep	 ): _nome(nome),
+				                        _endereco(endereco),
+							    	    _cidade(cidade),
+								        _estado(estado),
+								        _cep(cep) {}
+
+        // imprime na tela os dados de um cliente cadastrado
+		void print(); 
+
+	private:
+
+        // Dados do cadastro do cliente
+		std::string _nome;
+		std::string _endereco;
+		std::string _cidade;
+		std::string _estado;
+		std::string _cep;			
 };
 
 #endif
