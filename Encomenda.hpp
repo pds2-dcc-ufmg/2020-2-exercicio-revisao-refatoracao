96b116fe77fcefd6dfd63fd369d927bc83a4ee57
diegovieirdos <80367453+diegovieirdos@users.noreply.github.com>
2021-03-10 11:25:05 -0300

    Update Encomenda.hpp

--- Encomenda.hpp
+++ Encomenda.hpp
@@ -1,26 +1,26 @@
 #ifndef Encomenda_H
 #define Encomenda_H
-
+#include <iostream>
 #include "Cliente.hpp"
-using namespace std;
 
-class Encomenda{
+class Encomenda
+{
 
-	
-	protected:
+public:
 
-		double PESO = 0.0;
-		double CUSTOkg = 0.0;
-		double T = 0.0;
-		Cliente remetente;
-		Cliente dest;
-	
-	public:
-		Encomenda(double weight, double cost_per_kg, Cliente sender,  Cliente recipient): PESO(weight), CUSTOkg(cost_per_kg), remetente(sender) {}
-		virtual void print();
-		virtual double calcula();
-		// se normal retorna 1, se relampago retorna 2;
-		virtual int getClassType();
+	double peso = 0.0;
+	double custoKg = 0.0;
+	double custoTotal = 0.0;
+	Cliente remetente;
+	Cliente destinatario;
+
+		void print()
+		{
+        	std::cout << "[Remetente]" << std::endl;
+			remetente.print();
+			std::cout << "[Destinatário]" << std::endl;
+			destinatario.print();
+		}
 };
 
 #endif
